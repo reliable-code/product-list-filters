73f37fbfe4c01c116d89a274e3d1916e07038cd3
Stepan <stepan.saitov@gmail.com>
2023-09-26 15:13:20 +0500

    Prodoctorov init file

---
+++ prodoctorov.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Prodoctorov List Clean
+// @namespace    https://github.com/reliable-code/product-list-filters
+// @version      0.1
+// @description  Remove product cards by filter
+// @author       reliable-code
+// @license      MIT
+// @match        https://www.prodoctorov.ru/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=prodoctorov.ru
+// @grant        none
+// ==/UserScript==
+
+(function main() {
+}());
