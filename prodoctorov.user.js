c28c986e0ce046d5aa40a8b8d6e92b1c606ba7fd
Stepan <stepan.saitov@gmail.com>
2023-09-26 20:01:23 +0500

    Update version

--- prodoctorov.user.js
+++ prodoctorov.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Prodoctorov List Clean
 // @namespace    https://github.com/reliable-code/product-list-filters
-// @version      0.3.3
+// @version      0.4.0
 // @description  Remove product cards by filter
 // @author       reliable-code
 // @license      MIT
