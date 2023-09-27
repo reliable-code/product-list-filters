9566458590f6ca44b3d9e4fbacbf0c3b8b8ab596
Stepan <stepan.saitov@gmail.com>
2023-09-27 18:48:39 +0500

    Yamarket init file

---
+++ yamarket.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Yamarket List Clean
+// @namespace    https://github.com/reliable-code/product-list-filters
+// @version      0.1
+// @description  Remove product cards by filter
+// @author       reliable-code
+// @license      MIT
+// @match        https://market.yandex.ru/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=market.yandex.ru
+// @grant        none
+// ==/UserScript==
+
+(function main() {
+}());
