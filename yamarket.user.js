2a23295cd91f716a80efa626a26099608fd4b761
Stepan <stepan.saitov@gmail.com>
2023-09-27 20:38:09 +0500

    Add cleanList funtion

--- yamarket.user.js
+++ yamarket.user.js
@@ -14,6 +14,8 @@
     const MIN_REVIEWS = 5;
     const MIN_RATING = 4.8;
 
+    const PRODUCT_CARD_SNIPPET_SELECTOR = '[data-apiary-widget-name="@marketfront/Snippet"]';
+
     const CATEGORY_NAME = getCategoryName();
     const MIN_REVIEWS_LOCAL_STORAGE_KEY = `${CATEGORY_NAME}-min-reviews-filter`;
     const MIN_RATING_LOCAL_STORAGE_KEY = `${CATEGORY_NAME}-min-rating-filter`;
@@ -27,5 +29,39 @@
         const categoryName = pathElements[1] ?? 'common';
 
         return categoryName;
+    }
+    function cleanList() {
+        const productCardSnippets = document.querySelectorAll(PRODUCT_CARD_SNIPPET_SELECTOR);
+
+        productCardSnippets.forEach(
+            (productCardSnippet) => {
+                const productCard = productCardSnippet.parentNode.parentNode;
+
+                const ratingMeter = productCardSnippet.querySelector('[role="meter"]');
+
+                if (!ratingMeter) {
+                    productCard.remove();
+
+                    return;
+                }
+
+                const ratingInfoWrap = ratingMeter.parentNode;
+
+                const ratingInfoSpans = ratingInfoWrap.querySelectorAll(':scope > span');
+
+                const productCardReviews = ratingInfoSpans[1];
+                const productCardReviewsText = productCardReviews.innerText;
+                const productCardReviewsDigit = +productCardReviewsText;
+
+                const productCardRating = ratingInfoSpans[0];
+                const productCardRatingText = productCardRating.innerText;
+                const productCardRatingDigit = +productCardRatingText;
+
+                if (productCardReviewsDigit < minReviewsValue
+                    || productCardRatingDigit < minRatingValue) {
+                    productCard.remove();
+                }
+            },
+        );
     }
 }());
