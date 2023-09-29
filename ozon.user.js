e7541d0316b0d2b87440a7fa674b2fd73abd5e9e
Stepan <stepan.saitov@gmail.com>
2023-09-29 20:14:57 +0500

    Update version

--- ozon.user.js
+++ ozon.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Ozon List Clean
 // @namespace    https://github.com/reliable-code/product-list-filters
-// @version      0.6.1
+// @version      0.6.2
 // @description  Remove product cards by filter
 // @author       reliable-code
 // @license      MIT
