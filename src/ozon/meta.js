37651aeabff6fa0e9680b22b288b16253bdfdf16
Stepan <stepan.saitov@gmail.com>
2023-09-30 21:23:04 +0500

    Add meta file for ozon

---
+++ src/ozon/meta.js
@@ -0,0 +1,11 @@
+module.exports = {
+    name: 'Ozon List Clean',
+    version: '0.6.2',
+    match: ['https://www.ozon.ru/*'],
+    description: 'Remove product cards by filter',
+    icon: 'https://www.google.com/s2/favicons?sz=64&domain=ozon.ru',
+    grant: 'none',
+    author: 'reliable-code',
+    license: 'MIT',
+    namespace: 'https://github.com/reliable-code/product-list-filters',
+};
