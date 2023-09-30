f55bace45e7b83674116982036ca5dbde5d688df
Stepan <stepan.saitov@gmail.com>
2023-09-30 21:54:31 +0500

    Set monkey config to multiple userscripts

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,10 @@ const path = require('path');
 const { monkey } = require('webpack-monkey');
 
 module.exports = monkey({
-    entry: './src/ozon/index.js',
+    entry: {
+        ozon: './src/ozon/index.js',
+    },
     output: {
-        filename: 'ozon.user.js',
         path: path.resolve(__dirname, 'dist'),
     },
 });
