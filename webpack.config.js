c303521667bc582df459010177c27df7457a50c6
Stepan <stepan.saitov@gmail.com>
2023-09-30 22:34:22 +0500

    Add wb build config

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ module.exports = monkey({
         lavka: './src/lavka/index.js',
         ozon: './src/ozon/index.js',
         prodoctorov: './src/prodoctorov/index.js',
+        wb: './src/wb/index.js',
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
