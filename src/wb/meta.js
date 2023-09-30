af87901c3c6ec58c1013a880b3c14ec838d967a3
Stepan <stepan.saitov@gmail.com>
2023-09-30 22:09:43 +0500

    Add meta file for wb

---
+++ src/wb/meta.js
@@ -0,0 +1,11 @@
+module.exports = {
+    name: 'WB List Clean',
+    version: '0.5',
+    match: ['https://www.wildberries.ru/*'],
+    description: 'Remove product cards by filter',
+    icon: 'https://www.google.com/s2/favicons?sz=64&domain=wildberries.ru',
+    grant: 'none',
+    author: 'reliable-code',
+    license: 'MIT',
+    namespace: 'https://github.com/reliable-code/product-list-filters',
+};
