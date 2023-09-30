dc064bbe8bd5d667fd3dba10ce94bc17feb8f90e
Stepan <stepan.saitov@gmail.com>
2023-09-30 22:05:12 +0500

    Add meta file for prodoctorov

---
+++ src/prodoctorov/meta.js
@@ -0,0 +1,11 @@
+module.exports = {
+    name: 'Prodoctorov List Clean',
+    version: '0.4.1',
+    match: ['https://prodoctorov.ru/*'],
+    description: 'Remove profile cards by filter',
+    icon: 'https://www.google.com/s2/favicons?sz=64&domain=prodoctorov.ru',
+    grant: 'none',
+    author: 'reliable-code',
+    license: 'MIT',
+    namespace: 'https://github.com/reliable-code/product-list-filters',
+};
