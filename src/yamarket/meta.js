ccb108f63bacfbae7c6e2bac423f1123deb4a864
Stepan <stepan.saitov@gmail.com>
2023-09-30 22:35:48 +0500

    Add meta file for yamarket

---
+++ src/yamarket/meta.js
@@ -0,0 +1,11 @@
+module.exports = {
+    name: 'Yamarket List Clean',
+    version: '0.3.1',
+    match: ['https://market.yandex.ru/*'],
+    description: 'Remove product cards by filter',
+    icon: 'https://www.google.com/s2/favicons?sz=64&domain=market.yandex.ru',
+    grant: 'none',
+    author: 'reliable-code',
+    license: 'MIT',
+    namespace: 'https://github.com/reliable-code/product-list-filters',
+};
