d30f19a31d7837d3c337f5b1c792027608e2e789
Stepan <stepan.saitov@gmail.com>
2023-10-03 20:10:21 +0500

    Add filter controls for lenta

--- src/lenta/index.js
+++ src/lenta/index.js
@@ -1,4 +1,9 @@
-import { getAllElements, getElementInnerNumber, getFirstElement } from '../common/dom';
+import {
+    createDefaultFilterControl,
+    getAllElements,
+    getElementInnerNumber,
+    getFirstElement, updateInput
+} from '../common/dom';
 
 const MIN_RATING = 4.0;
 
@@ -13,9 +18,24 @@ setTimeout(() => {
     const productCardList = getFirstElement(document, PRODUCT_CARD_LIST_SELECTOR);
 
     if (productCardList) {
+        appendFilterControls(productCardList);
+
         setInterval(cleanList, 500);
     }
 }, 1000);
+
+function appendFilterControls(filtersBlockContainer) {
+    const controlStyle = '';
+
+    const minRatingDiv =
+        createDefaultFilterControl('Минимальный рейтинг: ', minRatingValue, '0.1', '4.0', '5.0', updateMinRatingInput, controlStyle);
+
+    filtersBlockContainer.prepend(minRatingDiv);
+}
+
+function updateMinRatingInput(e) {
+    updateInput(MIN_RATING_LOCAL_STORAGE_KEY, e);
+}
 
 function cleanList() {
     const productCards = getAllElements(document, PRODUCT_CARD_SELECTOR);
