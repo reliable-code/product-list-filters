90f4b2f80932b813bfd8b46245db25f6260c5a23
Stepan <stepan.saitov@gmail.com>
2023-10-03 20:17:37 +0500

    Rebuld scripts

--- dist/ozon.user.js
+++ dist/ozon.user.js
@@ -3,7 +3,7 @@
 // @description  Remove product cards by filter
 // @match        https://www.ozon.ru/*
 // @namespace    https://github.com/reliable-code/product-list-filters
-// @version      0.6.69634459
+// @version      0.6.69634624
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=ozon.ru
 // @author       reliable-code
 // ==/UserScript==
