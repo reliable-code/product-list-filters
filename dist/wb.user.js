3ef5297dda9132f1553fb918e41fcb9338224428
Stepan <stepan.saitov@gmail.com>
2023-10-03 16:12:31 +0500

    Rebuild scripts

--- dist/wb.user.js
+++ dist/wb.user.js
@@ -3,7 +3,7 @@
 // @description  Remove product cards by filter
 // @match        https://www.wildberries.ru/*
 // @namespace    https://github.com/reliable-code/product-list-filters
-// @version      0.5.69628086
+// @version      0.5.69633151
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=wildberries.ru
 // @author       reliable-code
 // ==/UserScript==
