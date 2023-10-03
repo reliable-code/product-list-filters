7524c9b9f9d55fae315ad1913a24f936893dfb62
Stepan <stepan.saitov@gmail.com>
2023-10-04 02:16:12 +0500

    Update versions

--- src/lenta/meta.js
+++ src/lenta/meta.js
@@ -3,7 +3,7 @@ const commonMeta = require('../common/meta');
 
 module.exports = {
     name: 'Lenta List Clean',
-    version: `0.1.${getUniqueTimestampSlice()}`,
+    version: `0.2.${getUniqueTimestampSlice()}`,
     match: ['https://*.online.lenta.com/*'],
     description: 'Remove product cards by filter',
     icon: 'https://www.google.com/s2/favicons?sz=64&domain=lenta.com',
