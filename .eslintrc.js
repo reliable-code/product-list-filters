3a93bee68267273133b8eb896eb17e845c6599be
Stepan <stepan.saitov@gmail.com>
2023-10-06 22:58:55 +0500

    Disable eslint no-param-reassign

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,33 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: 'airbnb-base',
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: [
-        '.eslintrc.{js,cjs}',
-      ],
-      parserOptions: {
-        sourceType: 'script',
-      },
+    env: {
+        browser: true,
+        es2021: true,
     },
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-  },
-  rules: {
-    indent: 'off',
-    'no-use-before-define': ['error', { functions: false, classes: false }],
-    'operator-linebreak': 'off',
-    'function-paren-newline': ['error', 'consistent'],
-  },
+    extends: 'airbnb-base',
+    overrides: [
+        {
+            env: {
+                node: true,
+            },
+            files: [
+                '.eslintrc.{js,cjs}',
+            ],
+            parserOptions: {
+                sourceType: 'script',
+            },
+        },
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest',
+    },
+    rules: {
+        indent: 'off',
+        'no-use-before-define': ['error', {
+            functions: false,
+            classes: false,
+        }],
+        'operator-linebreak': 'off',
+        'function-paren-newline': ['error', 'consistent'],
+        'no-param-reassign': 'off',
+    },
 };
