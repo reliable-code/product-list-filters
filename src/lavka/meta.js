25e104f2465fa05423dd057e947b84f9eb7670a7
Stepan <stepan.saitov@gmail.com>
2023-10-08 03:34:03 +0500

    Update version

--- src/lavka/meta.js
+++ src/lavka/meta.js
@@ -3,7 +3,7 @@ const commonMeta = require('../common/meta');
 
 module.exports = {
     name: 'Lavka List Clean',
-    version: `0.4.${getUniqueTimestampSlice()}`,
+    version: `0.5.${getUniqueTimestampSlice()}`,
     match: ['https://lavka.yandex.ru/*'],
     description: 'Remove product cards without discount',
     icon: 'https://www.google.com/s2/favicons?sz=64&domain=yandex.ru',
--- src/prodoctorov/meta.js
+++ src/prodoctorov/meta.js
@@ -3,7 +3,7 @@ const commonMeta = require('../common/meta');
 
 module.exports = {
     name: 'Prodoctorov List Clean',
-    version: `0.5.${getUniqueTimestampSlice()}`,
+    version: `0.6.${getUniqueTimestampSlice()}`,
     match: ['https://prodoctorov.ru/*'],
     description: 'Remove profile cards by filter',
     icon: 'https://www.google.com/s2/favicons?sz=64&domain=prodoctorov.ru',
