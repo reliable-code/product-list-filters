06aa26a5a24dad2354949cd302a20f4ea24d7c5c
Stepan <stepan.saitov@gmail.com>
2023-10-08 15:00:23 +0500

    Add removeSpaces function

--- src/common/string.js
+++ src/common/string.js
@@ -1,3 +1,7 @@
 export function removeNonDigit(stringValue) {
     return stringValue.replace(/\D/g, '');
 }
+
+export function removeSpaces(stringValue) {
+    return stringValue.replace(/\s/g, '');
+}
