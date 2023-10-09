8631a5b3a932da21bd2b4c1b825b4bb625677985
Stepan <stepan.saitov@gmail.com>
2023-10-09 21:06:11 +0500

    Add removeNonNumber function

--- src/common/string.js
+++ src/common/string.js
@@ -2,6 +2,10 @@ export function removeNonDigit(stringValue) {
     return stringValue.replace(/\D/g, '');
 }
 
+export function removeNonNumber(stringValue) {
+    return stringValue.replace(/[^\d.-]/g, '');
+}
+
 export function removeSpaces(stringValue) {
     return stringValue.replace(/\s/g, '');
 }
