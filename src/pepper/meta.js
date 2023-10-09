2e735f1d3e9c40a0ae29a7bf980a2f4e9921bb26
Stepan <stepan.saitov@gmail.com>
2023-10-10 00:24:33 +0500

    Update pepper version

--- src/pepper/meta.js
+++ src/pepper/meta.js
@@ -3,7 +3,7 @@ const commonMeta = require('../common/meta');
 
 module.exports = {
     name: 'Pepper List Clean',
-    version: `0.1.${getUniqueTimestampSlice()}`,
+    version: `0.2.${getUniqueTimestampSlice()}`,
     match: ['https://www.pepper.ru/*'],
     description: 'Remove product cards by filter',
     icon: 'https://www.google.com/s2/favicons?sz=64&domain=pepper.ru',
