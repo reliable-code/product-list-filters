ddec501526efcb3f08554a4bd5bf61b2e882ba33
Stepan <stepan.saitov@gmail.com>
2023-10-09 22:46:05 +0500

    Add showExpired filter for pepper

--- src/pepper/index.js
+++ src/pepper/index.js
@@ -6,16 +6,23 @@ import {
     showElement,
 } from '../common/dom';
 import { getStorageValueOrDefault, setStorageValueFromEvent } from '../common/storage';
-import { appendFilterControlsIfNeeded, createMinVotesFilterControl } from '../common/filter';
+import {
+    appendFilterControlsIfNeeded,
+    createFilterControlCheckbox,
+    createMinVotesFilterControl,
+} from '../common/filter';
 
 const MIN_VOTES = 100;
+const SHOW_EXPIRED = false;
 
 const MIN_VOTES_LOCAL_STORAGE_KEY = 'min-votes-filter';
+const SHOW_EXPIRED_LOCAL_STORAGE_KEY = 'show-expired-filter';
 const PRODUCT_CARD_LIST_SELECTOR = '.listLayout-main';
 const PRODUCT_CARD_SELECTOR = '.thread:not(.js-telegram-widget)';
 const PRODUCT_CARD_RATING_SELECTOR = '.vote-box > span';
 
 let minVotesValue = getStorageValueOrDefault(MIN_VOTES_LOCAL_STORAGE_KEY, MIN_VOTES);
+let showExpiredChecked = getStorageValueOrDefault(SHOW_EXPIRED_LOCAL_STORAGE_KEY, SHOW_EXPIRED);
 
 setInterval(initListClean, 500);
 
@@ -34,13 +41,23 @@ function appendFiltersContainer(filtersContainer, parentNode) {
 
     const controlStyle = '';
     const numberInputStyle = '';
+    const checkboxInputStyle = '';
 
     const minVotesDiv =
         createMinVotesFilterControl(
             minVotesValue, updateMinVotesValue, controlStyle, numberInputStyle,
         );
 
-    filtersContainer.append(minVotesDiv);
+    const showExpiredDiv =
+        createFilterControlCheckbox(
+            'Завершённые: ',
+            showExpiredChecked,
+            updateShowExpiredValue,
+            controlStyle,
+            checkboxInputStyle,
+        );
+
+    filtersContainer.append(minVotesDiv, showExpiredDiv);
 
     parentNode.prepend(filtersContainer);
 }
@@ -49,11 +66,23 @@ function updateMinVotesValue(e) {
     minVotesValue = setStorageValueFromEvent(e, MIN_VOTES_LOCAL_STORAGE_KEY);
 }
 
+function updateShowExpiredValue(e) {
+    showExpiredChecked = setStorageValueFromEvent(e, SHOW_EXPIRED_LOCAL_STORAGE_KEY);
+}
+
 function cleanList() {
     const productCards = getAllElements(PRODUCT_CARD_SELECTOR);
 
     productCards.forEach(
         (productCard) => {
+            const isExpired = productCard.classList.contains('thread--expired');
+
+            if (isExpired && !showExpiredChecked) {
+                hideElement(productCard);
+
+                return;
+            }
+
             const productCardRating = getFirstElement(PRODUCT_CARD_RATING_SELECTOR, productCard);
 
             const productCardRatingNumber = getElementInnerNumber(productCardRating, true);
