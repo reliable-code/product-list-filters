242b6ae97bd96a7f3c5814f3aecd915d0375af5b
Stepan <stepan.saitov@gmail.com>
2023-10-11 21:41:11 +0500

    Add filter enabling functionality to yamarket

--- src/yamarket/index.js
+++ src/yamarket/index.js
@@ -2,18 +2,22 @@ import {
     getAllElements,
     getArrayElementInnerNumber,
     getFirstElement,
+    hideElement,
     insertAfter,
+    showElement,
     showHideElement,
 } from '../common/dom';
 import { getStorageValueOrDefault, setStorageValueFromEvent } from '../common/storage';
 import {
     appendFilterControlsIfNeeded,
+    createEnabledFilterControl,
     createMinRatingFilterControl,
     createMinReviewsFilterControl,
 } from '../common/filter';
 
 const MIN_REVIEWS = 5;
 const MIN_RATING = 4.8;
+const FILTER_ENABLED = true;
 
 const SEARCH_CONTROLS_SELECTOR = '[data-apiary-widget-name="@marketplace/SearchControls"]';
 const VIRTUOSO_SCROLLER_SELECTOR = '[data-virtuoso-scroller="true"]';
@@ -23,9 +27,11 @@ const PRODUCT_CARD_PARENT_ATTRIBUTE = 'data-apiary-widget-name';
 const CATEGORY_NAME = getCategoryName();
 const MIN_REVIEWS_STORAGE_KEY = `${CATEGORY_NAME}-min-reviews-filter`;
 const MIN_RATING_STORAGE_KEY = `${CATEGORY_NAME}-min-rating-filter`;
+const FILTER_ENABLED_STORAGE_KEY = `${CATEGORY_NAME}-filter-enabled`;
 
 let minReviewsValue = getStorageValueOrDefault(MIN_REVIEWS_STORAGE_KEY, MIN_REVIEWS);
 let minRatingValue = getStorageValueOrDefault(MIN_RATING_STORAGE_KEY, MIN_RATING);
+let filterEnabledChecked = getStorageValueOrDefault(FILTER_ENABLED_STORAGE_KEY, FILTER_ENABLED);
 
 function getCategoryName() {
     const { pathname } = window.location;
@@ -40,34 +46,49 @@ const searchControls = getFirstElement(SEARCH_CONTROLS_SELECTOR);
 if (searchControls) {
     appendFilterControlsIfNeeded(searchControls, appendFiltersContainer);
 
-    setInterval(cleanList, 500);
+    setInterval(cleanList, 100);
 }
 
 function appendFiltersContainer(filterControls, parentNode) {
     filterControls.style =
         'display: flex;' +
-        'gap: 10px;' +
+        'gap: 15px;' +
         'padding: 0 10px 15px;' +
         'font-size: 16px;' +
         'font-weight: 500;';
 
+    const controlStyle =
+        'display: flex;' +
+        'align-items: center;';
     const inputStyle =
+        'margin: 0px 5px;';
+    const numberInputStyle =
+        inputStyle + // eslint-disable-line prefer-template
         'border-radius: 7px;' +
         'border: none;' +
         'padding: 9px 11px;' +
         'box-shadow: inset 0 0 0 1.5px #d2d0cc;';
+    const checkboxInputStyle =
+        inputStyle + // eslint-disable-line prefer-template
+        'width: 23px;' +
+        'height: 23px;';
 
     const minReviewsDiv =
         createMinReviewsFilterControl(
-            minReviewsValue, updateMinReviewsValue, '', inputStyle,
+            minReviewsValue, updateMinReviewsValue, controlStyle, numberInputStyle,
         );
 
     const minRatingDiv =
         createMinRatingFilterControl(
-            minRatingValue, updateMinRatingValue, '', inputStyle,
+            minRatingValue, updateMinRatingValue, controlStyle, numberInputStyle,
         );
 
-    filterControls.append(minReviewsDiv, minRatingDiv);
+    const filterEnabledDiv =
+        createEnabledFilterControl(
+            filterEnabledChecked, updateFilterEnabledValue, controlStyle, checkboxInputStyle,
+        );
+
+    filterControls.append(minReviewsDiv, minRatingDiv, filterEnabledDiv);
 
     insertAfter(parentNode, filterControls);
 }
@@ -78,6 +99,10 @@ function updateMinReviewsValue(e) {
 
 function updateMinRatingValue(e) {
     minRatingValue = setStorageValueFromEvent(e, MIN_RATING_STORAGE_KEY);
+}
+
+function updateFilterEnabledValue(e) {
+    filterEnabledChecked = setStorageValueFromEvent(e, FILTER_ENABLED_STORAGE_KEY);
 }
 
 function cleanList() {
@@ -99,10 +124,16 @@ function cleanList() {
                 ? productCardSnippetParent.parentNode.parentNode
                 : productCardSnippetParent;
 
+            if (!filterEnabledChecked) {
+                showElement(productCard);
+
+                return;
+            }
+
             const ratingMeter = getFirstElement('[role="meter"]', productCardSnippet);
 
             if (!ratingMeter) {
-                productCard.remove();
+                hideElement(productCard);
 
                 return;
             }
--- src/yamarket/meta.js
+++ src/yamarket/meta.js
@@ -3,7 +3,7 @@ const commonMeta = require('../common/meta');
 
 module.exports = {
     name: 'Yamarket List Clean',
-    version: `0.4.${getUniqueTimestampSlice()}`,
+    version: `0.5.${getUniqueTimestampSlice()}`,
     match: ['https://market.yandex.ru/*'],
     description: 'Remove product cards by filter',
     icon: 'https://www.google.com/s2/favicons?sz=64&domain=market.yandex.ru',
