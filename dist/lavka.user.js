4cdc95baa834c0543206902786564cf6a3c05b8a
Stepan <stepan.saitov@gmail.com>
2023-10-18 00:37:04 +0500

    Rebuild scripts

--- dist/lavka.user.js
+++ dist/lavka.user.js
@@ -3,7 +3,7 @@
 // @description  Remove product cards without discount
 // @match        https://lavka.yandex.ru/*
 // @namespace    https://github.com/reliable-code/product-list-filters
-// @version      0.6.69728665
+// @version      0.6.69757140
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=yandex.ru
 // @author       reliable-code
 // ==/UserScript==
