b21056158f6f3d5fa5e26ee9d39499262c4bd89c
Stepan <stepan.saitov@gmail.com>
2023-12-06 13:55:18 +0500

    Update webpack-monkey

--- dist/kazan.user.js
+++ dist/kazan.user.js
@@ -3,7 +3,7 @@
 // @description  Remove product cards by filter
 // @match        https://kazanexpress.ru/*
 // @namespace    https://github.com/reliable-code/product-list-filters
-// @version      0.1.70135167
+// @version      0.1.70185281
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=kazanexpress.ru
 // @author       reliable-code
 // ==/UserScript==
