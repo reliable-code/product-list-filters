1c1e522efe359acadff57e4288e5748baae72aa3
Stepan <stepan.saitov@gmail.com>
2024-01-23 17:06:03 +0500

    Add match for category pages

--- dist/vseins.user.js
+++ dist/vseins.user.js
@@ -1,9 +1,10 @@
 // ==UserScript==
 // @name         Vseins List Clean
 // @description  Remove product cards by filter
-// @match        https://vseinstrumenti.ru/*
+// @match        https://www.vseinstrumenti.ru/tag-page/*
+// @match        https://www.vseinstrumenti.ru/category/*
 // @namespace    https://github.com/reliable-code/product-list-filters
-// @version      0.1.70600148
+// @version      0.1.70601145
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=vseinstrumenti.ru
 // @author       reliable-code
 // ==/UserScript==
