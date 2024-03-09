9565b8d961588352e5efa5c57a6b4ef29e0709d1
Stepan <stepan.saitov@gmail.com>
2024-03-09 16:01:37 +0500

    Extract productList functions

--- src/ozon/index.js
+++ src/ozon/index.js
@@ -1,70 +1,31 @@
 import { thumbsDownIcon } from './icons';
 import {
-    addGlobalStyle,
     createDiv,
     createLink,
     createSpan,
-    debounce,
-    getAllElements,
-    getArrayElementInnerNumber,
     getFirstElement,
-    hideElement,
     insertAfter,
-    showElement,
-    showHideElement,
     waitForElement,
 } from '../common/dom';
-import { getStorageValue, setStorageValue, StoredInputValue } from '../common/storage';
+import { getStorageValue, setStorageValue } from '../common/storage';
 import { removeSpaces } from '../common/string';
-import {
-    appendFilterControlsIfNeeded,
-    createEnabledFilterControl,
-    createMaxReviewsFilterControl,
-    createMinRatingFilterControl,
-    createMinReviewsFilterControl,
-    createNameFilterControl,
-    createNoRatingFilterControl,
-    isGreaterThanFilter,
-    isLessThanFilter,
-    isNotMatchTextFilter,
-} from '../common/filter';
 import {
     appendPriceHistory,
     createDislikeButton,
-    getProductArticleFromLink,
     getProductArticleFromPathname,
     getStoredRatingValue,
-    PRODUCT_CARDS_SELECTOR,
     setStoredRatingValue,
 } from './common';
 import {
     initAppendStoredPriceValues as initAppendFavoritesStoredPriceValues,
-} from './favoritesPage';
+} from './favoritesList';
+import { initListClean, paginatorContent } from './productList';
 
-const PAGINATOR_CONTENT_SELECTOR = '#paginatorContent';
 const PRODUCT_REVIEWS_WRAP_OLD_SELECTOR = '[data-widget="webReviewProductScore"]';
 const PRODUCT_REVIEWS_WRAP_SELECTOR = '[data-widget="webSingleProductScore"]';
 const COMMENTS_SELECTOR = '#comments';
 
-const SEARCH_RESULTS_SORT_SELECTOR = '[data-widget="searchResultsSort"]';
-const PRODUCT_CARD_NAME_SELECTOR = '.tsBody500Medium';
-const PRODUCT_CARD_RATING_WRAP_SELECTOR = '.tsBodyMBold';
 const CREATE_REVIEW_BUTTON_SELECTOR = '[data-widget="createReviewButton"]';
-
-const CATEGORY_NAME = getCategoryName();
-
-const nameFilter =
-    new StoredInputValue(`${CATEGORY_NAME}-name-filter`, null, cleanList);
-const minReviewsFilter =
-    new StoredInputValue(`${CATEGORY_NAME}-min-reviews-filter`, null, cleanList);
-const maxReviewsFilter =
-    new StoredInputValue(`${CATEGORY_NAME}-max-reviews-filter`, null, cleanList);
-const minRatingFilter =
-    new StoredInputValue(`${CATEGORY_NAME}-min-rating-filter`, 4.8, cleanList);
-const noRatingFilter =
-    new StoredInputValue(`${CATEGORY_NAME}-no-rating-filter`, false, cleanList);
-const filterEnabled =
-    new StoredInputValue(`${CATEGORY_NAME}-filter-enabled`, true, cleanList);
 
 function migrateDatabase() {
     const newDBVersion = 2;
@@ -83,21 +44,6 @@ function migrateDatabase() {
     setStorageValue('dbVersion', newDBVersion);
 }
 
-function getCategoryName() {
-    const { pathname } = window.location;
-    const pathElements = pathname.split('/');
-
-    const fullCategoryName = pathElements[2];
-
-    if (!fullCategoryName) return 'common';
-
-    const categoryName = fullCategoryName.split('-')
-        .at(-1);
-
-    return categoryName;
-}
-
-const paginatorContent = getFirstElement(PAGINATOR_CONTENT_SELECTOR);
 const comments = getFirstElement(COMMENTS_SELECTOR);
 
 if (paginatorContent) {
@@ -108,191 +54,6 @@ if (paginatorContent) {
     initAppendFavoritesStoredPriceValues();
 } else {
     appendAdditionalProductPageControls();
-}
-
-function initListClean() {
-    waitForElement(document, SEARCH_RESULTS_SORT_SELECTOR)
-        .then((searchResultsSort) => {
-            appendFilterControlsIfNeeded(searchResultsSort, appendFiltersContainer);
-
-            window.addEventListener('storage', cleanList);
-
-            const observer = new MutationObserver(debounce(cleanList, 50));
-
-            observer.observe(paginatorContent, {
-                childList: true,
-                subtree: true,
-            });
-        });
-}
-
-function appendFiltersContainer(filtersContainer, parentNode) {
-    addGlobalStyle(
-        'input[type=number]::-webkit-inner-spin-button,' +
-        'input[type=number]::-webkit-outer-spin-button {' +
-        '    -webkit-appearance: auto;' +
-        '}',
-    );
-
-    filtersContainer.style =
-        'display: flex;' +
-        'grid-gap: 15px;' +
-        'margin-top: 14px;';
-
-    const controlStyle =
-        'display: flex;' +
-        'align-items: center;';
-    const inputStyle =
-        'margin-left: 5px;' +
-        'border: 2px solid #b3bcc5;' +
-        'border-radius: 6px;' +
-        'padding: 6px 10px;';
-    const textInputStyle =
-        inputStyle + // eslint-disable-line prefer-template
-        'width: 190px;';
-    const numberInputStyle =
-        inputStyle + // eslint-disable-line prefer-template
-        'width: 90px;';
-    const checkboxInputStyle =
-        'margin-left: 5px;' +
-        'width: 25px;' +
-        'height: 25px;';
-
-    const nameFilterDiv =
-        createNameFilterControl(nameFilter, controlStyle, textInputStyle);
-
-    const minReviewsDiv =
-        createMinReviewsFilterControl(minReviewsFilter, controlStyle, numberInputStyle);
-
-    const maxReviewsDiv =
-        createMaxReviewsFilterControl(maxReviewsFilter, controlStyle, numberInputStyle);
-
-    const minRatingDiv =
-        createMinRatingFilterControl(minRatingFilter, controlStyle, numberInputStyle);
-
-    const noRatingDiv =
-        createNoRatingFilterControl(noRatingFilter, controlStyle, checkboxInputStyle);
-
-    const filterEnabledDiv =
-        createEnabledFilterControl(filterEnabled, controlStyle, checkboxInputStyle);
-
-    filtersContainer.append(
-        nameFilterDiv, minReviewsDiv, maxReviewsDiv, minRatingDiv, noRatingDiv, filterEnabledDiv,
-    );
-
-    parentNode.append(filtersContainer);
-
-    parentNode.style =
-        'position: sticky;' +
-        'top: 62px;' +
-        'background-color: #fff;' +
-        'z-index: 2;' +
-        'padding-bottom: 11px;' +
-        'margin-bottom: 0;' +
-        'gap: 15px;';
-}
-
-function cleanList() {
-    const productCards = getAllElements(PRODUCT_CARDS_SELECTOR, paginatorContent);
-
-    productCards.forEach(
-        (productCard) => {
-            if (!filterEnabled.value) {
-                showElement(productCard, 'flex');
-
-                return;
-            }
-
-            const productCardLink =
-                getFirstElement('a', productCard);
-
-            if (!productCardLink) {
-                hideElement(productCard);
-                return;
-            }
-
-            const productArticle = getProductArticleFromLink(productCardLink);
-
-            const productCardNameWrap =
-                getFirstElement(PRODUCT_CARD_NAME_SELECTOR, productCard);
-
-            if (!productCardNameWrap) {
-                hideElement(productCard);
-                return;
-            }
-
-            const productCardRatingWrap =
-                getFirstElement(PRODUCT_CARD_RATING_WRAP_SELECTOR, productCard);
-
-            let productCardReviewsNumber;
-            let productCardRatingNumber;
-
-            if (!productCardRatingWrap) {
-                const anyRatingFilterHasValue =
-                    minReviewsFilter.value || maxReviewsFilter.value || minRatingFilter.value;
-
-                if (anyRatingFilterHasValue && !noRatingFilter.value) {
-                    hideElement(productCard);
-                    return;
-                }
-            } else {
-                const productCardRatingWrapSpans =
-                    getAllElements(':scope > span', productCardRatingWrap, true);
-
-                productCardReviewsNumber =
-                    getArrayElementInnerNumber(productCardRatingWrapSpans, 1, true);
-
-                const storedRatingValue = getStoredRatingValue(productArticle);
-
-                if (!storedRatingValue) {
-                    productCardRatingNumber =
-                        getArrayElementInnerNumber(productCardRatingWrapSpans, 0);
-                } else {
-                    productCardRatingNumber = storedRatingValue;
-
-                    productCardRatingWrapSpans[0].children[1].textContent =
-                        storedRatingValue.toString()
-                            .padEnd(5);
-                }
-
-                appendProductDislikeButtonIfNeeded(productCardRatingWrap, productArticle);
-            }
-
-            const productCardName = productCardNameWrap.innerText;
-
-            productCardNameWrap.title = productCardName;
-
-            const conditionToHide =
-                isNotMatchTextFilter(productCardName, nameFilter) ||
-                isLessThanFilter(productCardReviewsNumber, minReviewsFilter) ||
-                isGreaterThanFilter(productCardReviewsNumber, maxReviewsFilter) ||
-                isLessThanFilter(productCardRatingNumber, minRatingFilter);
-            showHideElement(productCard, conditionToHide, 'flex');
-        },
-    );
-}
-
-function appendProductDislikeButtonIfNeeded(productCardRatingWrap, productArticle) {
-    const productCardRatingWrapParent = productCardRatingWrap.parentNode;
-
-    if (productCardRatingWrapParent.hasAttribute('dislikeButtonAdded')) {
-        return;
-    }
-
-    productCardRatingWrapParent.style = 'display: flex; width: 100%';
-
-    const dislikeButton =
-        createDislikeButton(
-            () => dislikeProductOnProductList(productArticle), false,
-        );
-
-    productCardRatingWrapParent.append(dislikeButton);
-    productCardRatingWrapParent.setAttribute('dislikeButtonAdded', '');
-}
-
-function dislikeProductOnProductList(productArticle) {
-    setStoredRatingValue(productArticle, 1);
-    cleanList();
 }
 
 function isFavoritesPage() {
---
+++ src/ozon/productList.js
@@ -0,0 +1,252 @@
+import { StoredInputValue } from '../common/storage';
+import {
+    addGlobalStyle,
+    debounce,
+    getAllElements,
+    getArrayElementInnerNumber,
+    getFirstElement,
+    hideElement,
+    showElement,
+    showHideElement,
+    waitForElement,
+} from '../common/dom';
+import {
+    appendFilterControlsIfNeeded,
+    createEnabledFilterControl,
+    createMaxReviewsFilterControl,
+    createMinRatingFilterControl,
+    createMinReviewsFilterControl,
+    createNameFilterControl,
+    createNoRatingFilterControl,
+    isGreaterThanFilter,
+    isLessThanFilter,
+    isNotMatchTextFilter,
+} from '../common/filter';
+import {
+    createDislikeButton,
+    getProductArticleFromLink,
+    getStoredRatingValue,
+    PRODUCT_CARDS_SELECTOR,
+    setStoredRatingValue,
+} from './common';
+
+const PAGINATOR_CONTENT_SELECTOR = '#paginatorContent';
+export const paginatorContent = getFirstElement(PAGINATOR_CONTENT_SELECTOR);
+
+const SEARCH_RESULTS_SORT_SELECTOR = '[data-widget="searchResultsSort"]';
+const PRODUCT_CARD_NAME_SELECTOR = '.tsBody500Medium';
+const PRODUCT_CARD_RATING_WRAP_SELECTOR = '.tsBodyMBold';
+
+const CATEGORY_NAME = getCategoryName();
+
+const nameFilter =
+    new StoredInputValue(`${CATEGORY_NAME}-name-filter`, null, cleanList);
+const minReviewsFilter =
+    new StoredInputValue(`${CATEGORY_NAME}-min-reviews-filter`, null, cleanList);
+const maxReviewsFilter =
+    new StoredInputValue(`${CATEGORY_NAME}-max-reviews-filter`, null, cleanList);
+const minRatingFilter =
+    new StoredInputValue(`${CATEGORY_NAME}-min-rating-filter`, 4.8, cleanList);
+const noRatingFilter =
+    new StoredInputValue(`${CATEGORY_NAME}-no-rating-filter`, false, cleanList);
+const filterEnabled =
+    new StoredInputValue(`${CATEGORY_NAME}-filter-enabled`, true, cleanList);
+
+function getCategoryName() {
+    const { pathname } = window.location;
+    const pathElements = pathname.split('/');
+
+    const fullCategoryName = pathElements[2];
+
+    if (!fullCategoryName) return 'common';
+
+    const categoryName = fullCategoryName.split('-')
+        .at(-1);
+
+    return categoryName;
+}
+
+export function initListClean() {
+    waitForElement(document, SEARCH_RESULTS_SORT_SELECTOR)
+        .then((searchResultsSort) => {
+            appendFilterControlsIfNeeded(searchResultsSort, appendFiltersContainer);
+
+            window.addEventListener('storage', cleanList);
+
+            const observer = new MutationObserver(debounce(cleanList, 50));
+
+            observer.observe(paginatorContent, {
+                childList: true,
+                subtree: true,
+            });
+        });
+}
+
+function appendFiltersContainer(filtersContainer, parentNode) {
+    addGlobalStyle(
+        'input[type=number]::-webkit-inner-spin-button,' +
+        'input[type=number]::-webkit-outer-spin-button {' +
+        '    -webkit-appearance: auto;' +
+        '}',
+    );
+
+    filtersContainer.style =
+        'display: flex;' +
+        'grid-gap: 15px;' +
+        'margin-top: 14px;';
+
+    const controlStyle =
+        'display: flex;' +
+        'align-items: center;';
+    const inputStyle =
+        'margin-left: 5px;' +
+        'border: 2px solid #b3bcc5;' +
+        'border-radius: 6px;' +
+        'padding: 6px 10px;';
+    const textInputStyle =
+        inputStyle + // eslint-disable-line prefer-template
+        'width: 190px;';
+    const numberInputStyle =
+        inputStyle + // eslint-disable-line prefer-template
+        'width: 90px;';
+    const checkboxInputStyle =
+        'margin-left: 5px;' +
+        'width: 25px;' +
+        'height: 25px;';
+
+    const nameFilterDiv =
+        createNameFilterControl(nameFilter, controlStyle, textInputStyle);
+
+    const minReviewsDiv =
+        createMinReviewsFilterControl(minReviewsFilter, controlStyle, numberInputStyle);
+
+    const maxReviewsDiv =
+        createMaxReviewsFilterControl(maxReviewsFilter, controlStyle, numberInputStyle);
+
+    const minRatingDiv =
+        createMinRatingFilterControl(minRatingFilter, controlStyle, numberInputStyle);
+
+    const noRatingDiv =
+        createNoRatingFilterControl(noRatingFilter, controlStyle, checkboxInputStyle);
+
+    const filterEnabledDiv =
+        createEnabledFilterControl(filterEnabled, controlStyle, checkboxInputStyle);
+
+    filtersContainer.append(
+        nameFilterDiv, minReviewsDiv, maxReviewsDiv, minRatingDiv, noRatingDiv, filterEnabledDiv,
+    );
+
+    parentNode.append(filtersContainer);
+
+    parentNode.style =
+        'position: sticky;' +
+        'top: 62px;' +
+        'background-color: #fff;' +
+        'z-index: 2;' +
+        'padding-bottom: 11px;' +
+        'margin-bottom: 0;' +
+        'gap: 15px;';
+}
+
+function cleanList() {
+    const productCards = getAllElements(PRODUCT_CARDS_SELECTOR, paginatorContent);
+
+    productCards.forEach(
+        (productCard) => {
+            if (!filterEnabled.value) {
+                showElement(productCard, 'flex');
+
+                return;
+            }
+
+            const productCardLink =
+                getFirstElement('a', productCard);
+
+            if (!productCardLink) {
+                hideElement(productCard);
+                return;
+            }
+
+            const productArticle = getProductArticleFromLink(productCardLink);
+
+            const productCardNameWrap =
+                getFirstElement(PRODUCT_CARD_NAME_SELECTOR, productCard);
+
+            if (!productCardNameWrap) {
+                hideElement(productCard);
+                return;
+            }
+
+            const productCardRatingWrap =
+                getFirstElement(PRODUCT_CARD_RATING_WRAP_SELECTOR, productCard);
+
+            let productCardReviewsNumber;
+            let productCardRatingNumber;
+
+            if (!productCardRatingWrap) {
+                const anyRatingFilterHasValue =
+                    minReviewsFilter.value || maxReviewsFilter.value || minRatingFilter.value;
+
+                if (anyRatingFilterHasValue && !noRatingFilter.value) {
+                    hideElement(productCard);
+                    return;
+                }
+            } else {
+                const productCardRatingWrapSpans =
+                    getAllElements(':scope > span', productCardRatingWrap, true);
+
+                productCardReviewsNumber =
+                    getArrayElementInnerNumber(productCardRatingWrapSpans, 1, true);
+
+                const storedRatingValue = getStoredRatingValue(productArticle);
+
+                if (!storedRatingValue) {
+                    productCardRatingNumber =
+                        getArrayElementInnerNumber(productCardRatingWrapSpans, 0);
+                } else {
+                    productCardRatingNumber = storedRatingValue;
+
+                    productCardRatingWrapSpans[0].children[1].textContent =
+                        storedRatingValue.toString()
+                            .padEnd(5);
+                }
+
+                appendProductDislikeButtonIfNeeded(productCardRatingWrap, productArticle);
+            }
+
+            const productCardName = productCardNameWrap.innerText;
+
+            productCardNameWrap.title = productCardName;
+
+            const conditionToHide =
+                isNotMatchTextFilter(productCardName, nameFilter) ||
+                isLessThanFilter(productCardReviewsNumber, minReviewsFilter) ||
+                isGreaterThanFilter(productCardReviewsNumber, maxReviewsFilter) ||
+                isLessThanFilter(productCardRatingNumber, minRatingFilter);
+            showHideElement(productCard, conditionToHide, 'flex');
+        },
+    );
+}
+
+function appendProductDislikeButtonIfNeeded(productCardRatingWrap, productArticle) {
+    const productCardRatingWrapParent = productCardRatingWrap.parentNode;
+
+    if (productCardRatingWrapParent.hasAttribute('dislikeButtonAdded')) {
+        return;
+    }
+
+    productCardRatingWrapParent.style = 'display: flex; width: 100%';
+
+    const dislikeButton =
+        createDislikeButton(
+            () => dislikeProductOnProductList(productArticle), false,
+        );
+
+    productCardRatingWrapParent.append(dislikeButton);
+    productCardRatingWrapParent.setAttribute('dislikeButtonAdded', '');
+}
+
+function dislikeProductOnProductList(productArticle) {
+    setStoredRatingValue(productArticle, 1);
+    cleanList();
+}
