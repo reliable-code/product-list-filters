2bc2f2992fcc51014ca21f205b7050500ef04ea6
Stepan <stepan.saitov@gmail.com>
2024-05-02 13:52:14 +0500

    Add coupon processing for addBalancedCashbackPriceIfNeeded function

--- src/mm/pages/common/common.js
+++ src/mm/pages/common/common.js
@@ -2,32 +2,41 @@ import { getElementInnerNumber, getFirstElement } from '../../../common/dom';
 
 export const PRICE_ATTR = 'price';
 export const BALANCED_CASHBACK_PRICE_ATTR = 'balanced-cashback-price';
+export const COUPON_ATTR = 'applied-coupon';
 
-export function addBalancedCashbackPriceIfNeeded(priceParent, priceSelector, cashbackNumber) {
+export function addBalancedCashbackPriceIfNeeded(
+    priceParent, priceSelector, cashbackNumber, couponValue,
+) {
     const priceElement = getFirstElement(priceSelector, priceParent);
+    const couponValueChanged = checkCouponValueChanged(priceElement, couponValue);
 
-    if (!priceElement.hasAttribute(BALANCED_CASHBACK_PRICE_ATTR)) {
-        addBalancedCashbackPrice(priceElement, cashbackNumber);
+    if (couponValueChanged || !priceElement.hasAttribute(BALANCED_CASHBACK_PRICE_ATTR)) {
+        addBalancedCashbackPrice(priceElement, cashbackNumber, couponValue);
     }
 
     return priceElement;
 }
 
-function addBalancedCashbackPrice(priceElement, cashbackNumber) {
-    const priceNumber =
-        getElementInnerNumber(priceElement, true);
+function addBalancedCashbackPrice(priceElement, cashbackNumber, couponValue) {
+    let priceNumber = getPriceNumber(priceElement);
+    priceElement.setAttribute(PRICE_ATTR, priceNumber);
+
+    if (couponValue) {
+        priceNumber -= couponValue;
+        priceElement.setAttribute(COUPON_ATTR, couponValue);
+    } else {
+        priceElement.removeAttribute(COUPON_ATTR);
+    }
 
     const balancedCashbackUsage = getBalancedCashbackUsage(priceNumber, cashbackNumber);
 
     const balancedCashbackPrice = priceNumber - balancedCashbackUsage;
+    priceElement.setAttribute(BALANCED_CASHBACK_PRICE_ATTR, balancedCashbackPrice);
 
     const newPriceElementText =
         `${priceNumber.toLocaleString()} (${balancedCashbackPrice.toLocaleString()}) ₽`;
     priceElement.innerText = newPriceElementText;
     priceElement.title = `-${balancedCashbackUsage.toLocaleString()}`;
-
-    priceElement.setAttribute(PRICE_ATTR, priceNumber);
-    priceElement.setAttribute(BALANCED_CASHBACK_PRICE_ATTR, balancedCashbackPrice);
 }
 
 function getPriceNumber(priceElement) {
