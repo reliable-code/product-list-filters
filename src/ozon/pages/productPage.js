992ad198a7e6817e0044b2aacb6712419a611fda
Stepan <stepan.saitov@gmail.com>
2024-06-05 19:08:20 +0500

    Extract executeProductPageMods

--- src/ozon/pages/productPage.js
+++ src/ozon/pages/productPage.js
@@ -22,6 +22,10 @@ const PRODUCT_REVIEWS_WRAP_SELECTOR = '[data-widget="webSingleProductScore"]';
 const CREATE_REVIEW_BUTTON_SELECTOR = '[data-widget="createReviewButton"]';
 
 export function initProductPageMods() {
+    executeProductPageMods();
+}
+
+function executeProductPageMods() {
     waitForElement(document, `${PRODUCT_REVIEWS_WRAP_OLD_SELECTOR}, ${PRODUCT_REVIEWS_WRAP_SELECTOR}`)
         .then((productReviewsWrap) => {
             if (!productReviewsWrap) return;
