a12ed992786ada42dddcaee84833d98594491537
Stepan <stepan.saitov@gmail.com>
2024-11-17 02:08:08 +0500

    Add appendPriceHistory initial function

--- src/wb/pages/productPage.js
+++ src/wb/pages/productPage.js
@@ -16,12 +16,18 @@ function initAppendPriceHistory() {
         .then((priceBlock) => {
             if (!priceBlock) return;
 
-            const price = getFirstElementInnerNumber(priceBlock, PRICE_SELECTOR, true);
-
-            console.log(price);
+            const productArticle = getProductArticleFromPathname();
+            appendPriceHistory(priceBlock, productArticle);
         });
 }
 
 function getProductArticleFromPathname() {
     return getURLPathElementEnding(2, 'unknown');
+}
+
+function appendPriceHistory(priceBlock, productArticle) {
+    const price = getFirstElementInnerNumber(priceBlock, PRICE_SELECTOR, true);
+
+    console.log(price);
+    console.log(productArticle);
 }
