58a1026baa410da8b40fd083724735b8239210f7
Stepan <stepan.saitov@gmail.com>
2024-11-17 03:46:52 +0500

    Get and set product from storage

--- src/wb/pages/common/common.js
+++ src/wb/pages/common/common.js
@@ -1,10 +1,18 @@
 import { getElementInnerNumber, getFirstElement } from '../../../common/dom';
+import { getStorageValue, setStorageValue } from '../../../common/storage';
+import { ProductData } from '../../../common/models/productData';
 
 export function appendPriceHistory(priceContainer, priceSelector, productArticle) {
     const priceSpan = getFirstElement(priceSelector, priceContainer);
     const currentPriceValue = getElementInnerNumber(priceSpan, true);
 
-    console.log(currentPriceValue);
-    console.log(productArticle);
+    const productStorageKey = `product-${productArticle}`;
+    let currentProduct = getStorageValue(productStorageKey);
+
+    if (!currentProduct) {
+        currentProduct = new ProductData();
+    }
+
+    setStorageValue(productStorageKey, currentProduct);
 }
 
