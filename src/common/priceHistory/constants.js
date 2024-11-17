e671cfc8c88341f5db89149e95e24ad0281c9c7f
Stepan <stepan.saitov@gmail.com>
2024-11-18 01:18:38 +0500

    Add APPEND_PRICE_HISTORY_PASSED_ATTR const

--- src/common/priceHistory/constants.js
+++ src/common/priceHistory/constants.js
@@ -1,3 +1,4 @@
+export const APPEND_PRICE_HISTORY_PASSED_ATTR = 'appendPriceHistoryPassed';
 export const APPEND_STORED_PRICE_VALUES_PASSED_ATTR = 'appendStoredPriceValuesPassed';
 export const CURRENT_PRICE_ATTR = 'currentPrice';
 export const LOWEST_PRICE_ATTR = 'lowestPrice';
