b4790233e9471f2755b9819ce97f9c85e6287577
Stepan <stepan.saitov@gmail.com>
2024-11-17 23:53:56 +0500

    Extend PRODUCT_CARDS_SELECTOR

--- src/wb/pages/favorites.js
+++ src/wb/pages/favorites.js
@@ -29,8 +29,8 @@ import {
 import { checkIfGoodPrice } from '../../common/priceHistory/manipulation';
 
 const FILTER_CONTAINER_SELECTOR = '.favorites-goods__head';
-const PRODUCT_CARDS_SELECTOR = '.goods-card';
 const PRODUCT_LIST_CONTAINER_SELECTOR = '.favorites-goods';
+const PRODUCT_CARDS_SELECTOR = '.favorites-goods__list > .goods-card';
 const PRICE_SELECTOR = '.wallet-price';
 
 const nameFilter =
