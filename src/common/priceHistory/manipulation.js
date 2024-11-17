f8676a19ffc6315bcbd5f32f2bad7eb8f74cea55
Stepan <stepan.saitov@gmail.com>
2024-11-17 23:41:33 +0500

    Move checkIfGoodPrice to file

---
+++ src/common/priceHistory/manipulation.js
@@ -0,0 +1,27 @@
+import { CURRENT_PRICE_ATTR, GOOD_PRICE_ATTR, LOWEST_PRICE_ATTR } from './constants';
+
+export function checkIfGoodPrice(priceContainerWrap, productCard, priceTolerancePercentValue) {
+    const currentPrice = productCard.getAttribute(CURRENT_PRICE_ATTR);
+    const lowestPrice = productCard.getAttribute(LOWEST_PRICE_ATTR);
+
+    const priceToleranceFactor = 1 + (priceTolerancePercentValue / 100);
+    const goodPrice = lowestPrice * priceToleranceFactor;
+
+    if (currentPrice <= goodPrice) {
+        priceContainerWrap.style.border = '3px solid rgb(214, 245, 177)';
+        priceContainerWrap.style.borderRadius = '14px';
+        priceContainerWrap.style.padding = '4px 10px 6px';
+        priceContainerWrap.style.marginBottom = '5px';
+        priceContainerWrap.style.width = '-webkit-fill-available';
+
+        productCard.setAttribute(GOOD_PRICE_ATTR, '');
+    } else {
+        const stylePropertiesToRemove =
+            ['border', 'borderRadius', 'padding', 'marginBottom', 'width'];
+        stylePropertiesToRemove.forEach(
+            (property) => priceContainerWrap.style.removeProperty(property),
+        );
+
+        productCard.removeAttribute(GOOD_PRICE_ATTR);
+    }
+}
--- src/ozon/pages/favorites.js
+++ src/ozon/pages/favorites.js
@@ -30,6 +30,7 @@ import {
     GOOD_PRICE_ATTR,
     LOWEST_PRICE_ATTR,
 } from '../../common/priceHistory/constants';
+import { checkIfGoodPrice } from '../../common/priceHistory/manipulation';
 
 const PAGINATOR_SELECTOR = '[data-widget="paginator"]';
 
@@ -183,30 +184,4 @@ function appendStoredPriceValues(priceContainer, productCard, priceContainerWrap
     priceContainerWrap.style.display = 'block';
 
     productCard.setAttribute(APPEND_STORED_PRICE_VALUES_PASSED_ATTR, '');
-}
-
-function checkIfGoodPrice(priceContainerWrap, productCard, priceTolerancePercentValue) {
-    const currentPrice = productCard.getAttribute(CURRENT_PRICE_ATTR);
-    const lowestPrice = productCard.getAttribute(LOWEST_PRICE_ATTR);
-
-    const priceToleranceFactor = 1 + (priceTolerancePercentValue / 100);
-    const goodPrice = lowestPrice * priceToleranceFactor;
-
-    if (currentPrice <= goodPrice) {
-        priceContainerWrap.style.border = '3px solid rgb(214, 245, 177)';
-        priceContainerWrap.style.borderRadius = '14px';
-        priceContainerWrap.style.padding = '4px 10px 6px';
-        priceContainerWrap.style.marginBottom = '5px';
-        priceContainerWrap.style.width = '-webkit-fill-available';
-
-        productCard.setAttribute(GOOD_PRICE_ATTR, '');
-    } else {
-        const stylePropertiesToRemove =
-            ['border', 'borderRadius', 'padding', 'marginBottom', 'width'];
-        stylePropertiesToRemove.forEach(
-            (property) => priceContainerWrap.style.removeProperty(property),
-        );
-
-        productCard.removeAttribute(GOOD_PRICE_ATTR);
-    }
 }
