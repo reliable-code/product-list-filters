49b5b9f33ab916f8e0885a8f58c00d90ee95d6bb
Stepan <stepan.saitov@gmail.com>
2024-11-17 23:44:29 +0500

    Call favorites mods from index

--- src/wb/index.js
+++ src/wb/index.js
@@ -1,5 +1,6 @@
 import { initProductListMods } from './pages/productList';
 import { initProductPageMods } from './pages/productPage';
+import { initFavoritesMods } from './pages/favorites';
 import { pathnameIncludes, somePathElementEquals } from '../common/url';
 
 if (somePathElementEquals('catalog') || somePathElementEquals('brands')) {
@@ -8,4 +9,6 @@ if (somePathElementEquals('catalog') || somePathElementEquals('brands')) {
     } else {
         initProductListMods();
     }
+} else if (somePathElementEquals('favorites')) {
+    initFavoritesMods();
 }
