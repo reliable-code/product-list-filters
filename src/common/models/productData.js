038604d161d0064abcf723bd822e1ddc7cacbfc7
Stepan <stepan.saitov@gmail.com>
2024-11-17 15:30:22 +0500

    Default ProductData price values are 0

--- src/common/models/productData.js
+++ src/common/models/productData.js
@@ -1,6 +1,6 @@
 export class ProductData {
     constructor(
-        lastCheckDate = new Date().toLocaleDateString(), lowestPrice = null, highestPrice = null,
+        lastCheckDate = new Date().toLocaleDateString(), lowestPrice = 0, highestPrice = 0,
     ) {
         this.lastCheckDate = lastCheckDate;
         this.lowestPrice = lowestPrice;
