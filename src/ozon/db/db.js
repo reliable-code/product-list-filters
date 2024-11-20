5d41cab25a9273e6d97c1f9449f76b6f5592c27f
Stepan <stepan.saitov@gmail.com>
2024-11-20 19:27:58 +0500

    Add migration to new structure

--- src/ozon/db/db.js
+++ src/ozon/db/db.js
@@ -1,13 +1,42 @@
-import { deleteMigrationTask, runMigrationTaskIfNeeded } from '../../common/db/db';
+import { processAllEntries, runMigrationTaskIfNeeded } from '../../common/db/db';
+import { getStorageValue, setStorageValue } from '../../common/storage/storage';
+import { RatedProductData as ProductData } from '../../common/models/ratedProductData';
 
-const ACTUAL_DB_VERSION = 4;
+const ACTUAL_DB_VERSION = 5;
 
 export function runMigration() {
-    runMigrationTaskIfNeeded(migrationTask, ACTUAL_DB_VERSION);
+    runMigrationTaskIfNeeded(migrationTask, ACTUAL_DB_VERSION, false);
 }
 
 function migrationTask() {
-    const keyFilterCondition = (key) => key.includes('filter');
+    // const keyFilterCondition = (key) => key.includes('filter');
+    const processEntry = (key, value) => {
+        if (key.endsWith('-lp')) {
+            updateProduct(key, 'lowestPrice', value);
+        }
+        if (key.endsWith('-hp')) {
+            updateProduct(key, 'highestPrice', value);
+        }
+        if (key.endsWith('-rate')) {
+            updateProduct(key, 'rating', value);
+        }
+    };
 
-    deleteMigrationTask(keyFilterCondition);
+    processAllEntries(processEntry, true);
+}
+
+function updateProduct(entryKey, productPropKey, value) {
+    const [productId] = entryKey.split('-');
+
+    const productStorageKey = `product-${productId}`;
+    const storedProduct = getStorageValue(productStorageKey);
+
+    const currentProduct =
+        storedProduct ? ProductData.fromObject(storedProduct) : new ProductData();
+
+    currentProduct[productPropKey] = value;
+
+    setStorageValue(productStorageKey, currentProduct);
+
+    window.GM_deleteValue(entryKey);
 }
