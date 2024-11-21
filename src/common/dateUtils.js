704a3abb83f023d658dd7d0fbc549c077ce229d6
Stepan <stepan.saitov@gmail.com>
2024-11-21 23:44:42 +0500

    Add getDateTimestamp

--- src/common/dateUtils.js
+++ src/common/dateUtils.js
@@ -1,3 +1,9 @@
+export function getDateTimestamp() {
+    const timestamp = Date.now();
+    const msInDay = 86400000;
+    return timestamp - (timestamp % msInDay);
+}
+
 export function getLocalDateFromTimestamp(timestamp) {
     return new Date(timestamp).toLocaleDateString();
 }
