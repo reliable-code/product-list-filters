e7dfdc8d23187fd37a9d546466d9ade5d700d584
Stepan <stepan.saitov@gmail.com>
2024-11-22 14:54:33 +0500

    Add createRow function

--- src/common/dom/tableFactory.js
+++ src/common/dom/tableFactory.js
@@ -3,3 +3,7 @@ import { createStyledElement } from './elementsFactory';
 export function createTable(styles = {}) {
     return createStyledElement('table', styles);
 }
+
+export function createRow(styles = {}) {
+    return createStyledElement('tr', styles);
+}
