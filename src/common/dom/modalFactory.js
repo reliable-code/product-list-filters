b1eee226d12c006e662b8f35584398ccfa4f3fca
Stepan <stepan.saitov@gmail.com>
2024-11-22 20:38:20 +0500

    Add createCloseButton and DEFAULT_CLOSE_BUTTON_STYLES

--- src/common/dom/modalFactory.js
+++ src/common/dom/modalFactory.js
@@ -1,3 +1,24 @@
+import { createButton } from './elementsFactory';
+
+const DEFAULT_CLOSE_BUTTON_STYLES = {
+    position: 'absolute',
+    top: '0px',
+    right: '8px',
+    fontSize: '24px',
+    cursor: 'pointer',
+    color: '#000',
+};
+
+export function createCloseButton(modal, styles = DEFAULT_CLOSE_BUTTON_STYLES) {
+    const closeButton = createButton(styles, '×');
+
+    closeButton.addEventListener('click', () => {
+        closeModal(modal);
+    });
+
+    return closeButton;
+}
+
 export function handleClickOutside(event, modal) {
     if (!modal.contains(event.target)) {
         closeModal(modal);
