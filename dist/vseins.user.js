096a90946b3ac42f370ce2df5d2347e589acfa10
Stepan <stepan.saitov@gmail.com>
2024-11-24 03:33:05 +0500

    Rebuild

--- dist/vseins.user.js
+++ dist/vseins.user.js
@@ -4,7 +4,7 @@
 // @match        https://www.vseinstrumenti.ru/tag-page/*
 // @match        https://www.vseinstrumenti.ru/category/*
 // @namespace    https://github.com/reliable-code/product-list-filters
-// @version      0.1.73238532
+// @version      0.1.73240117
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=vseinstrumenti.ru
 // @author       reliable-code
 // ==/UserScript==
