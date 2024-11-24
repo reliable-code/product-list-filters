02c99727114b89c79cb97680ba057b3490de34b7
Stepan <stepan.saitov@gmail.com>
2024-11-24 13:54:25 +0500

    Add getMedian

---
+++ src/common/mathUtils.js
@@ -0,0 +1,11 @@
+function getMedian(values) {
+    const sortedValues = values.slice()
+        .sort((a, b) => a - b);
+    const midIndex = Math.floor(sortedValues.length / 2);
+
+    if (sortedValues.length % 2 !== 0) {
+        return sortedValues[midIndex];
+    }
+
+    return (sortedValues[midIndex - 1] + sortedValues[midIndex]) / 2;
+}
