55ca3a41f7c662a613f58a0b2fe6f748702ae04a
Stepan <stepan.saitov@gmail.com>
2024-11-24 16:11:20 +0500

    Rebuild scripts

--- dist/pepper.user.js
+++ dist/pepper.user.js
@@ -3,7 +3,7 @@
 // @description  Remove product cards by filter
 // @match        https://www.pepper.ru/*
 // @namespace    https://github.com/reliable-code/product-list-filters
-// @version      0.3.73240117
+// @version      0.3.73244666
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=pepper.ru
 // @author       reliable-code
 // ==/UserScript==
