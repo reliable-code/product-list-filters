1b2d214438956e00077997b158a047a23c49b5fc
Stepan <stepan.saitov@gmail.com>
2024-11-26 17:00:39 +0500

    Change lenta site

--- src/lenta/meta.js
+++ src/lenta/meta.js
@@ -4,7 +4,7 @@ const commonMeta = require('../common/meta');
 module.exports = {
     name: 'Lenta List Clean',
     version: `0.3.${getUniqueTimestampSlice()}`,
-    match: ['https://*.online.lenta.com/*'],
+    match: ['https://*.lenta.com/*'],
     description: 'Remove product cards by filter',
     icon: 'https://www.google.com/s2/favicons?sz=64&domain=lenta.com',
     ...commonMeta,
