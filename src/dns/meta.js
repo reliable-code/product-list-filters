39ea98757a595430fc48bbc6e8298e97f3c99cd8
Stepan <stepan.saitov@gmail.com>
2024-11-29 23:00:31 +0500

    Update meta

--- src/dns/meta.js
+++ src/dns/meta.js
@@ -2,10 +2,10 @@ const { getUniqueTimestampSlice } = require('../common/utils');
 const commonMeta = require('../common/meta');
 
 module.exports = {
-    name: 'DNS List Clean',
+    name: 'DNS product cards filter',
     version: `0.1.${getUniqueTimestampSlice()}`,
     match: ['https://www.dns-shop.ru/catalog/*'],
-    description: 'Remove product cards by filter',
+    description: 'Hide product cards by filter',
     icon: 'https://www.google.com/s2/favicons?sz=64&domain=dns-shop.ru',
     ...commonMeta,
 };
