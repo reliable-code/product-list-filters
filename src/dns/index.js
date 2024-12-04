34023b56913b41c1a8d8ada87b5fbbe4102703ab
Stepan <stepan.saitov@gmail.com>
2024-12-04 16:22:59 +0500

    Move styles to file

--- src/dns/index.js
+++ src/dns/index.js
@@ -12,42 +12,7 @@ import {
     createSearchFilterControl,
 } from '../common/filter/factories/specificControls';
 import { SELECTORS } from './selectors';
-
-const STYLES_BASE = {
-    INPUT: {
-        marginLeft: '6px',
-        border: '1px solid #d9d9d9',
-        borderRadius: '8px',
-        padding: '7px 14px',
-        fontSize: '15px',
-    },
-};
-const STYLES = {
-    FILTERS_CONTAINER: {
-        display: 'flex',
-        gridGap: '12px',
-        paddingBottom: '12px',
-    },
-    CONTROL: {
-        display: 'flex',
-        alignItems: 'center',
-        fontSize: '15px',
-    },
-    INPUT: STYLES_BASE.INPUT,
-    TEXT_INPUT: {
-        ...STYLES_BASE.INPUT,
-        width: '180px',
-    },
-    NUMBER_INPUT: {
-        ...STYLES_BASE.INPUT,
-        width: '90px',
-    },
-    CHECKBOX_INPUT: {
-        marginLeft: '6px',
-        width: '21px',
-        height: '21px',
-    },
-};
+import { STYLES } from './styles';
 
 const SECTION_ID = getURLPathElement(3);
 
---
+++ src/dns/styles.js
@@ -0,0 +1,35 @@
+const STYLES_BASE = {
+    INPUT: {
+        marginLeft: '6px',
+        border: '1px solid #d9d9d9',
+        borderRadius: '8px',
+        padding: '7px 14px',
+        fontSize: '15px',
+    },
+};
+export const STYLES = {
+    FILTERS_CONTAINER: {
+        display: 'flex',
+        gridGap: '12px',
+        paddingBottom: '12px',
+    },
+    CONTROL: {
+        display: 'flex',
+        alignItems: 'center',
+        fontSize: '15px',
+    },
+    INPUT: STYLES_BASE.INPUT,
+    TEXT_INPUT: {
+        ...STYLES_BASE.INPUT,
+        width: '180px',
+    },
+    NUMBER_INPUT: {
+        ...STYLES_BASE.INPUT,
+        width: '90px',
+    },
+    CHECKBOX_INPUT: {
+        marginLeft: '6px',
+        width: '21px',
+        height: '21px',
+    },
+};
