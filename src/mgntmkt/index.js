080e26a4c33a35cedc6e372408f0ac4c14a0d8e0
Stepan <stepan.saitov@gmail.com>
2024-12-04 21:25:29 +0500

    Move selectors to file

--- src/mgntmkt/index.js
+++ src/mgntmkt/index.js
@@ -17,16 +17,7 @@ import {
     createMinReviewsFilterControl,
     createSearchFilterControl,
 } from '../common/filter/factories/specificControls';
-
-const SELECTORS = {
-    NOTIFICATION: '.notification',
-    PROMOTIONAL_SHELF: '.promotional-shelf',
-    CATEGORY_PRODUCTS: '#category-products',
-    PRODUCT_CARD: ':scope > div:not(.products-controllers)',
-    PRODUCT_CARD_NAME_WRAP: '.subtitle-item',
-    PRODUCT_CARD_RATING_WRAP: '.orders',
-    PRODUCT_CARD_RATING: '[data-test-id="text__rating"]',
-};
+import { SELECTORS } from './selectors';
 
 let nameFilter;
 let minReviewsFilter;
---
+++ src/mgntmkt/selectors.js
@@ -0,0 +1,9 @@
+export const SELECTORS = {
+    NOTIFICATION: '.notification',
+    PROMOTIONAL_SHELF: '.promotional-shelf',
+    CATEGORY_PRODUCTS: '#category-products',
+    PRODUCT_CARD: ':scope > div:not(.products-controllers)',
+    PRODUCT_CARD_NAME_WRAP: '.subtitle-item',
+    PRODUCT_CARD_RATING_WRAP: '.orders',
+    PRODUCT_CARD_RATING: '[data-test-id="text__rating"]',
+};
