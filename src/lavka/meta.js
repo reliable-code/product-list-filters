4e8942c7fc8281c996102a1888cae75828da0839
Stepan <stepan.saitov@gmail.com>
2024-12-04 18:51:49 +0500

    Update meta

--- src/lavka/meta.js
+++ src/lavka/meta.js
@@ -2,10 +2,10 @@ const { getUniqueTimestampSlice } = require('../common/utils');
 const commonMeta = require('../common/meta');
 
 module.exports = {
-    name: 'Lavka List Clean',
+    name: 'Lavka product cards filter',
     version: `0.6.${getUniqueTimestampSlice()}`,
     match: ['https://lavka.yandex.ru/*'],
-    description: 'Remove product cards without discount',
+    description: 'Hide product cards by filter',
     icon: 'https://www.google.com/s2/favicons?sz=64&domain=yandex.ru',
     ...commonMeta,
 };
