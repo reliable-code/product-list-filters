5cd7fe0090786ef70114558fc14de2d6663194f6
Stepan <stepan.saitov@gmail.com>
2024-12-05 18:52:44 +0500

    Update meta

--- src/mm/meta.js
+++ src/mm/meta.js
@@ -2,10 +2,10 @@ const { getUniqueTimestampSlice } = require('../common/utils');
 const commonMeta = require('../common/meta');
 
 module.exports = {
-    name: 'Megamarket List Clean',
+    name: 'MegaMarket product cards & offers filter',
     version: `0.1.${getUniqueTimestampSlice()}`,
     match: ['https://megamarket.ru/*'],
-    description: 'Remove product cards by filter',
+    description: 'Hide product cards & offers by filter',
     icon: 'https://www.google.com/s2/favicons?sz=64&domain=megamarket.ru',
     ...commonMeta,
 };
