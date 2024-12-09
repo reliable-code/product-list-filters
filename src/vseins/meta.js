897ff21f04cc5f15c2c7eda1eafaa2c9aa17c333
Stepan <stepan.saitov@gmail.com>
2024-12-10 00:45:42 +0500

    Update meta

--- src/vseins/meta.js
+++ src/vseins/meta.js
@@ -2,10 +2,10 @@ const { getUniqueTimestampSlice } = require('../common/utils');
 const commonMeta = require('../common/meta');
 
 module.exports = {
-    name: 'Vseins List Clean',
+    name: 'Vseins product cards filter',
     version: `0.1.${getUniqueTimestampSlice()}`,
     match: ['https://www.vseinstrumenti.ru/tag-page/*', 'https://www.vseinstrumenti.ru/category/*'],
-    description: 'Remove product cards by filter',
+    description: 'Hide product cards by filter',
     icon: 'https://www.google.com/s2/favicons?sz=64&domain=vseinstrumenti.ru',
     ...commonMeta,
 };
