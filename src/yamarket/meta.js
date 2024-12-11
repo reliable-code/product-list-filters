1250d023e01e4a61978c505a9130ca1066ca831d
Stepan <stepan.saitov@gmail.com>
2024-12-11 21:53:37 +0500

    Update meta

--- src/yamarket/meta.js
+++ src/yamarket/meta.js
@@ -2,10 +2,10 @@ const { getUniqueTimestampSlice } = require('../common/utils');
 const commonMeta = require('../common/meta');
 
 module.exports = {
-    name: 'Yamarket List Clean',
+    name: 'Yamarket product cards filter',
     version: `0.5.${getUniqueTimestampSlice()}`,
     match: ['https://market.yandex.ru/*'],
-    description: 'Remove product cards by filter',
+    description: 'Hide product cards by filter',
     icon: 'https://www.google.com/s2/favicons?sz=64&domain=market.yandex.ru',
     ...commonMeta,
 };
