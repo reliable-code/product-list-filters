1da15770f9fbd0a2d8fda6dd7ec96257e7206d55
Stepan <stepan.saitov@gmail.com>
2024-12-13 03:45:00 +0500

    Move selectors to file

--- src/prodoc/pages/doctorList/index.js
+++ src/prodoc/pages/doctorList/index.js
@@ -19,17 +19,7 @@ import {
     createMinReviewsFilterControl,
 } from '../../../common/filter/factories/specificControls';
 import { STYLES } from './styles';
-
-const SELECTORS = {
-    DOCTOR_CARD: '.b-doctor-card',
-    DOCTOR_CARD_NAME: '.b-doctor-card__name-surname',
-    PROFILE_CARD: '.b-profile-card',
-    REVIEWS_LINK: ':scope > a',
-    EXPERIENCE_WRAP: '.b-doctor-card__experience .mr-2 .ui-text',
-    SPEC_WRAP: '.b-doctor-card__spec',
-    CLINIC_CONTAINER: 'div.b-doctor-card__lpu-select',
-    CLINIC_WRAP: '.b-select__trigger-main-text',
-};
+import { SELECTORS } from './selectors';
 
 const SECTION_ID = getURLPathElement(2);
 
---
+++ src/prodoc/pages/doctorList/selectors.js
@@ -0,0 +1,10 @@
+export const SELECTORS = {
+    DOCTOR_CARD: '.b-doctor-card',
+    DOCTOR_CARD_NAME: '.b-doctor-card__name-surname',
+    PROFILE_CARD: '.b-profile-card',
+    REVIEWS_LINK: ':scope > a',
+    EXPERIENCE_WRAP: '.b-doctor-card__experience .mr-2 .ui-text',
+    SPEC_WRAP: '.b-doctor-card__spec',
+    CLINIC_CONTAINER: 'div.b-doctor-card__lpu-select',
+    CLINIC_WRAP: '.b-select__trigger-main-text',
+};
