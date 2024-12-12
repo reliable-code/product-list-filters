19f1524481340503ff202142aba87a7654d42de4
Stepan <stepan.saitov@gmail.com>
2024-12-12 22:03:51 +0500

    Add name filter

--- src/yamkt/index.js
+++ src/yamkt/index.js
@@ -1,7 +1,7 @@
 import { debounce } from '../common/dom/utils';
 import { StoredInputValue } from '../common/storage/localstorage';
 import { appendFilterControlsIfNeeded } from '../common/filter/manager';
-import { isLessThanFilter } from '../common/filter/compare';
+import { isLessThanFilter, isNotMatchTextFilter } from '../common/filter/compare';
 import {
     hideElement,
     insertAfter,
@@ -18,6 +18,7 @@ import {
     createEnabledFilterControl,
     createMinRatingFilterControl,
     createMinReviewsFilterControl,
+    createSearchFilterControl,
 } from '../common/filter/factories/specificControls';
 import { STYLES } from './styles';
 import { getHashOrDefault } from '../common/hash/helpers';
@@ -32,6 +33,7 @@ function createFilter(filterName, defaultValue = null, onChange = processProduct
     );
 }
 
+const nameFilter = createFilter('name-filter');
 const minReviewsFilter = createFilter('min-reviews-filter');
 const minRatingFilter = createFilter('min-rating-filter', 4.8);
 const filterEnabled = createFilter('filter-enabled', true);
@@ -58,6 +60,9 @@ export function initProductListMods() {
 function appendFiltersContainer(filterControls, parentNode) {
     applyStyles(filterControls, STYLES.FILTER_CONTROLS);
 
+    const nameFilterDiv = createSearchFilterControl(
+        nameFilter, STYLES.CONTROL, STYLES.TEXT_INPUT,
+    );
     const minReviewsDiv = createMinReviewsFilterControl(
         minReviewsFilter, STYLES.CONTROL, STYLES.NUMBER_INPUT,
     );
@@ -68,7 +73,9 @@ function appendFiltersContainer(filterControls, parentNode) {
         filterEnabled, STYLES.CONTROL, STYLES.CHECKBOX_INPUT,
     );
 
-    filterControls.append(minReviewsDiv, minRatingDiv, filterEnabledDiv);
+    filterControls.append(
+        nameFilterDiv, minReviewsDiv, minRatingDiv, filterEnabledDiv,
+    );
 
     insertAfter(parentNode, filterControls);
 }
@@ -85,18 +92,19 @@ function processProductCard(productCard) {
         return;
     }
 
-    const productCardName = getFirstElement(
+    const productCardNameWrap = getFirstElement(
         SELECTORS.PRODUCT_CARD_NAME, productCard,
     );
     const productCardReviewsWrap = getFirstElement(
         SELECTORS.PRODUCT_CARD_REVIEWS_WRAP, productCard,
     );
 
-    if (!productCardName || !productCardReviewsWrap) {
+    if (!productCardNameWrap || !productCardReviewsWrap) {
         hideElement(productCard);
         return;
     }
 
+    const productCardName = productCardNameWrap.innerText;
     const productCardReviewsNumber = getElementInnerNumber(
         productCardReviewsWrap.children[1], true,
     );
@@ -105,6 +113,7 @@ function processProductCard(productCard) {
     );
 
     const shouldHide =
+        isNotMatchTextFilter(productCardName, nameFilter) ||
         isLessThanFilter(productCardReviewsNumber, minReviewsFilter) ||
         isLessThanFilter(productCardRatingNumber, minRatingFilter);
     updateElementDisplay(productCard, shouldHide);
