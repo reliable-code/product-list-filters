a0d7f34ac7b130695163f33ccc6a3d08484b5e6b
Stepan <stepan.saitov@gmail.com>
2024-12-13 00:06:45 +0500

    Extract selectors to object

--- src/prodoctorov/pages/doctorPage.js
+++ src/prodoctorov/pages/doctorPage.js
@@ -2,7 +2,17 @@ import { getAllElements, getFirstElement } from '../../common/dom/helpers';
 import { createDiv, createLink } from '../../common/dom/factories/elements';
 import { appendAdditionalLinks } from './common/common';
 
-const DOCTOR_DETAILS_MENU_SELECTOR = '.b-doctor-details__toc';
+const SELECTORS = {
+    DOCTOR_DETAILS: '.b-doctor-details__toc',
+    DOCTOR_CARD_NAME: '.b-doctor-intro__title-first-line [itemprop="name"]',
+    DOCTOR_INTRO_LEFT: '.b-doctor-intro__left-side',
+    REVIEWS_FILTER: '.reviews-filter:not(.b-reviews-page__filter)',
+    NAME_SPAN_HOLDER: '.b-doctor-intro__title-first-line',
+    NAME_SPAN: '[itemprop="name"]',
+    REVIEWS_FILTER_SPAN: ':scope > span',
+    DOCTOR_CONTACTS: '#doctor-contacts',
+    MORE_REVIEWS_BUTTON: '[data-qa="show_more_list_items"]',
+};
 
 export function initDoctorPageMods() {
     appendDoctorPageAdditionalLinks();
@@ -12,42 +22,40 @@ export function initDoctorPageMods() {
 }
 
 function appendDoctorPageAdditionalLinks() {
-    const doctorCardName = getFirstElement('.b-doctor-intro__title-first-line [itemprop="name"]', document, true);
-    if (doctorCardName) {
-        const doctorName = doctorCardName.innerText.trim();
-        const linksContainer = createDiv();
-        linksContainer.style.textAlign = 'center';
-        const doctorIntroLeft = getFirstElement('.b-doctor-intro__left-side');
-        doctorIntroLeft.append(linksContainer);
+    const doctorCardName = getFirstElement(SELECTORS.DOCTOR_CARD_NAME, document, true);
+    if (!doctorCardName) return;
 
-        appendAdditionalLinks(doctorName, linksContainer);
-    }
+    const doctorName = doctorCardName.innerText.trim();
+    const linksContainer = createDiv();
+    linksContainer.style.textAlign = 'center';
+    const doctorIntroLeft = getFirstElement(SELECTORS.DOCTOR_INTRO_LEFT);
+    doctorIntroLeft.append(linksContainer);
+    appendAdditionalLinks(doctorName, linksContainer);
 }
 
 function appendReviewsInfoToHeader() {
-    const reviewsFilter =
-        getFirstElement('.reviews-filter:not(.b-reviews-page__filter)');
-
+    const reviewsFilter = getFirstElement(SELECTORS.REVIEWS_FILTER);
     if (!reviewsFilter) return;
 
-    const nameSpanHolder =
-        getFirstElement('.b-doctor-intro__title-first-line', document, true);
-
+    const nameSpanHolder = getFirstElement(SELECTORS.NAME_SPAN_HOLDER, document, true);
     if (!nameSpanHolder) return;
 
-    const nameSpan = getFirstElement('[itemprop="name"]', nameSpanHolder, true);
-
+    const nameSpan = getFirstElement(SELECTORS.NAME_SPAN, nameSpanHolder, true);
     if (!nameSpan) return;
 
     const reviewsInfo = createDiv();
     reviewsInfo.style.position = 'absolute';
     reviewsInfo.classList.add('v-application');
+
     const reviewsInfoWrap = createDiv();
     reviewsInfoWrap.style.position = 'relative';
     reviewsInfoWrap.style.height = '45px';
+
     reviewsInfoWrap.append(reviewsInfo);
 
-    const reviewsFilterSpans = getAllElements(':scope > span', reviewsFilter, true);
+    const reviewsFilterSpans = getAllElements(
+        SELECTORS.REVIEWS_FILTER_SPAN, reviewsFilter, true,
+    );
 
     const lastReviewsFilterSpansIndex = reviewsFilterSpans.length - 1;
 
@@ -69,12 +77,10 @@ function scrollToParentAndClick(element) {
 }
 
 function appendDoctorContactLink() {
-    const doctorContacts = getFirstElement('#doctor-contacts');
-
+    const doctorContacts = getFirstElement(SELECTORS.DOCTOR_CONTACTS);
     if (!doctorContacts) return;
 
-    const doctorDetailsMenu = [...getAllElements(DOCTOR_DETAILS_MENU_SELECTOR)].pop();
-
+    const doctorDetailsMenu = [...getAllElements(SELECTORS.DOCTOR_DETAILS)].pop();
     if (!doctorDetailsMenu) return;
 
     doctorDetailsMenu.style.position = 'sticky';
@@ -82,15 +88,17 @@ function appendDoctorContactLink() {
 
     const doctorContactsLinkTitle = createDiv({}, 'Место работы');
     doctorContactsLinkTitle.classList.add('b-doctor-details__toc-title');
+
     const doctorContactsLink = createLink({}, null, '#doctor-contacts');
-    doctorContactsLink.append(doctorContactsLinkTitle);
     doctorContactsLink.classList.add('b-doctor-details__toc-item');
+
+    doctorContactsLink.append(doctorContactsLinkTitle);
 
     doctorDetailsMenu.insertBefore(doctorContactsLink, doctorDetailsMenu.firstChild);
 }
 
 function clickMoreReviewsButtonWhileExists() {
-    const moreReviewsButton = getFirstElement('[data-qa="show_more_list_items"]');
+    const moreReviewsButton = getFirstElement(SELECTORS.MORE_REVIEWS_BUTTON);
 
     if (moreReviewsButton && !moreReviewsButton.classList.contains('d-none')) {
         moreReviewsButton.click();
