d06bff07efb2ba5aaddff5efbc26ad18b9e852de
Stepan <stepan.saitov@gmail.com>
2024-12-13 01:54:08 +0500

    Move selectors to file

--- src/prodoctorov/pages/doctorPage/index.js
+++ src/prodoctorov/pages/doctorPage/index.js
@@ -1,18 +1,7 @@
 import { getAllElements, getFirstElement } from '../../../common/dom/helpers';
 import { createDiv, createLink } from '../../../common/dom/factories/elements';
 import { appendAdditionalLinks } from '../common/common';
-
-const SELECTORS = {
-    DOCTOR_DETAILS: '.b-doctor-details__toc',
-    DOCTOR_CARD_NAME: '.b-doctor-intro__title-first-line [itemprop="name"]',
-    DOCTOR_INTRO_LEFT: '.b-doctor-intro__left-side',
-    REVIEWS_FILTER: '.reviews-filter:not(.b-reviews-page__filter)',
-    NAME_SPAN_HOLDER: '.b-doctor-intro__title-first-line',
-    NAME_SPAN: '[itemprop="name"]',
-    REVIEWS_FILTER_SPAN: ':scope > span',
-    DOCTOR_CONTACTS: '#doctor-contacts',
-    MORE_REVIEWS_BUTTON: '[data-qa="show_more_list_items"]',
-};
+import { SELECTORS } from './selectors';
 
 export function initDoctorPageMods() {
     appendDoctorPageAdditionalLinks();
---
+++ src/prodoctorov/pages/doctorPage/selectors.js
@@ -0,0 +1,11 @@
+export const SELECTORS = {
+    DOCTOR_DETAILS: '.b-doctor-details__toc',
+    DOCTOR_CARD_NAME: '.b-doctor-intro__title-first-line [itemprop="name"]',
+    DOCTOR_INTRO_LEFT: '.b-doctor-intro__left-side',
+    REVIEWS_FILTER: '.reviews-filter:not(.b-reviews-page__filter)',
+    NAME_SPAN_HOLDER: '.b-doctor-intro__title-first-line',
+    NAME_SPAN: '[itemprop="name"]',
+    REVIEWS_FILTER_SPAN: ':scope > span',
+    DOCTOR_CONTACTS: '#doctor-contacts',
+    MORE_REVIEWS_BUTTON: '[data-qa="show_more_list_items"]',
+};
