99013a7ca388154bd603d332da255f7fb57af8a9
Stepan <stepan.saitov@gmail.com>
2024-12-14 01:47:15 +0500

    Update meta

--- src/ozon/meta.js
+++ src/ozon/meta.js
@@ -2,10 +2,10 @@ const { getUniqueTimestampSlice } = require('../common/utils');
 const commonMeta = require('../common/meta');
 
 module.exports = {
-    name: 'Ozon List Clean',
-    version: `1.3.${getUniqueTimestampSlice()}`,
+    name: 'Ozon enhancer',
+    version: `2.0.${getUniqueTimestampSlice()}`,
     match: ['https://www.ozon.ru/*'],
-    description: 'Remove product cards by filter',
+    description: 'Hide product cards by filter, enhance product page, store price history',
     icon: 'https://www.google.com/s2/favicons?sz=64&domain=ozon.ru',
     ...commonMeta,
 };
