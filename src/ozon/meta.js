ae122feed83de047a30b46e9123bde9c50e91139
Stepan <stepan.saitov@gmail.com>
2024-12-14 13:24:52 +0500

    Update meta

--- src/ozon/meta.js
+++ src/ozon/meta.js
@@ -5,7 +5,7 @@ module.exports = {
     name: 'Ozon enhancer',
     version: `2.0.${getUniqueTimestampSlice()}`,
     match: ['https://www.ozon.ru/*'],
-    description: 'Hide product cards by filter, enhance product page, store price history',
+    description: 'Hide product cards by filter, enhance product page, store price history & rating',
     icon: 'https://www.google.com/s2/favicons?sz=64&domain=ozon.ru',
     ...commonMeta,
 };
--- src/wb/meta.js
+++ src/wb/meta.js
@@ -2,10 +2,10 @@ const { getUniqueTimestampSlice } = require('../common/utils');
 const commonMeta = require('../common/meta');
 
 module.exports = {
-    name: 'WB List Clean',
+    name: 'WB enhancer',
     version: `0.7.${getUniqueTimestampSlice()}`,
     match: ['https://www.wildberries.ru/*'],
-    description: 'Remove product cards by filter',
+    description: 'Hide product cards by filter, store price history',
     icon: 'https://www.google.com/s2/favicons?sz=64&domain=wildberries.ru',
     ...commonMeta,
 };
