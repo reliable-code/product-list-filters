23eda8ca02234e4d12e91146d8afdc0880bda499
Stepan <stepan.saitov@gmail.com>
2024-12-18 17:53:28 +0500

    Remove obsolete constants

--- src/wb/pages/common/index.js
+++ src/wb/pages/common/index.js
@@ -1,24 +1,6 @@
 import { getPathnameElement } from '../../../common/url';
 import { getFirstElement } from '../../../common/dom/helpers';
 
-export const PRODUCT_CARD_NAME_SELECTOR = '.favorites-card__brand-wrap';
-
-const INPUT_STYLE =
-    'margin-left: 4px;';
-export const NUMBER_INPUT_STYLE =
-    INPUT_STYLE + // eslint-disable-line prefer-template
-    'width: 60px;';
-export const TEXT_INPUT_STYLE =
-    INPUT_STYLE + // eslint-disable-line prefer-template
-    'width: 180px;';
-export const CONTROL_STYLE =
-    'display: flex;' +
-    'align-items: center;';
-export const CHECKBOX_INPUT_STYLE =
-    'margin-left: 5px;' +
-    'width: 25px;' +
-    'height: 25px;';
-
 export function getProductArticleFromLink(productCardLink) {
     const productCardLinkHref = productCardLink.getAttribute('href');
 
