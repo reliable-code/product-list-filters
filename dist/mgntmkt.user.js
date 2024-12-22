29a77e6848eb87a6aa93b2792b7d9f025a6a77ea
Stepan <stepan.saitov@gmail.com>
2024-12-22 15:33:40 +0500

    Rebuild scripts

--- dist/mgntmkt.user.js
+++ dist/mgntmkt.user.js
@@ -6,7 +6,7 @@
 // @grant        GM_getValue
 // @match        https://mm.ru/*
 // @namespace    https://github.com/reliable-code/product-list-filters
-// @version      0.1.73480461
+// @version      0.1.73486360
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=kazanexpress.ru
 // @author       reliable-code
 // ==/UserScript==
--- dist/prodoc.user.js
+++ dist/prodoc.user.js
@@ -6,7 +6,7 @@
 // @grant        GM_getValue
 // @match        https://prodoctorov.ru/*
 // @namespace    https://github.com/reliable-code/product-list-filters
-// @version      0.6.73480461
+// @version      0.6.73486360
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=prodoctorov.ru
 // @author       reliable-code
 // ==/UserScript==
