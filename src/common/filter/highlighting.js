a39ca82a6740d06d2498131ea7e36d3f5a7924ea
Stepan <stepan.saitov@gmail.com>
2025-01-03 19:14:16 +0500

    Add highlightSearchStringsByFilterMultiple

--- src/common/filter/highlighting.js
+++ src/common/filter/highlighting.js
@@ -5,3 +5,8 @@ export function highlightSearchStringsByFilter(filter, textWrap) {
     const searchStrings = getIncludedSearchStrings(filter);
     highlightSearchStrings(searchStrings, textWrap);
 }
+
+export function highlightSearchStringsByFilterMultiple(filter, textWraps) {
+    const searchStrings = getIncludedSearchStrings(filter);
+    textWraps.forEach((textWrap) => highlightSearchStrings(searchStrings, textWrap));
+}
