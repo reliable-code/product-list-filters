192c93f2077b7a113d3f49fd00bbd5d1463a1523
Stepan <stepan.saitov@gmail.com>
2025-01-03 23:23:43 +0500

    Add filters for reviews page

---
+++ src/wb/pages/reviews/index.js
@@ -0,0 +1,157 @@
+import { debounce, waitForElement } from '../../../common/dom/utils';
+import { appendFilterControlsIfNeeded } from '../../../common/filter/manager';
+import { applyStyles, getAllElements, getFirstElement } from '../../../common/dom/helpers';
+import {
+    createEnabledFilterControl,
+    createMaxRatingFilterControl,
+    createMinRatingFilterControl,
+    createSearchFilterControl,
+} from '../../../common/filter/factories/specificControls';
+import { STYLES } from '../common/styles';
+import { SELECTORS } from './selectors';
+import { createFilterFactory } from '../../../common/filter/factories/createFilter';
+import { hideElement, showElement, updateElementDisplay } from '../../../common/dom/manipulation';
+import {
+    isGreaterThanFilter,
+    isLessThanFilter,
+    isNotMatchTextFilter,
+} from '../../../common/filter/compare';
+import { addScrollToFiltersButton, getProductArticleFromPathname } from '../common';
+import { getReviewsLastProductArticle, setReviewsLastProductArticle } from '../../db/db';
+import { removeHighlights } from '../../../common/dom/highlighting';
+import { highlightSearchStringsByFilterMultiple } from '../../../common/filter/highlighting';
+
+const { createGlobalFilter } = createFilterFactory(processReviewCards);
+
+const textFilter = createGlobalFilter('reviews-text-filter');
+const minRatingFilter = createGlobalFilter('reviews-min-rating-filter');
+const maxRatingFilter = createGlobalFilter('reviews-max-rating-filter');
+const filterEnabled = createGlobalFilter('reviews-filter-enabled', true);
+
+export async function initReviewsMods() {
+    cleanFiltersIfNotLastProduct();
+
+    const controlsContainer = await waitForElement(document, SELECTORS.CONTROLS_CONTAINER);
+    appendFilterControlsIfNeeded(controlsContainer, appendFiltersContainer);
+
+    processReviewCards();
+
+    const observer = new MutationObserver(debounce(processReviewCards));
+    const reviewsContainer = getFirstElement(SELECTORS.REVIEWS_LIST);
+    observer.observe(reviewsContainer, {
+        childList: true,
+    });
+}
+
+function cleanFiltersIfNotLastProduct() {
+    const productArticle = getProductArticleFromPathname();
+    const lastProductArticle = getReviewsLastProductArticle();
+
+    if (productArticle !== lastProductArticle) {
+        textFilter.clearValue();
+        minRatingFilter.clearValue();
+        maxRatingFilter.clearValue();
+    }
+
+    setReviewsLastProductArticle(productArticle);
+}
+
+function appendFiltersContainer(filtersContainer, parentNode) {
+    applyStyles(filtersContainer, STYLES.REVIEWS_FILTERS_CONTAINER);
+    filtersContainer.classList.add('input-search');
+
+    const nameFilterDiv = createSearchFilterControl(
+        textFilter, STYLES.CONTROL, STYLES.TEXT_INPUT,
+    );
+    const minRatingDiv = createMinRatingFilterControl(
+        minRatingFilter,
+        STYLES.CONTROL,
+        STYLES.NUMBER_INPUT,
+        1,
+        1,
+    );
+    const maxRatingDiv = createMaxRatingFilterControl(
+        maxRatingFilter,
+        STYLES.CONTROL,
+        STYLES.NUMBER_INPUT,
+        1,
+        1,
+    );
+    const filterEnabledDiv = createEnabledFilterControl(
+        filterEnabled, STYLES.CONTROL, STYLES.CHECKBOX_INPUT,
+    );
+
+    filtersContainer.append(
+        nameFilterDiv,
+        minRatingDiv,
+        maxRatingDiv,
+        filterEnabledDiv,
+    );
+
+    parentNode.append(filtersContainer);
+    addScrollToFiltersButton();
+}
+
+function processReviewCards() {
+    const reviewCards = getAllElements(SELECTORS.REVIEWS);
+    reviewCards.forEach(processReviewCard);
+
+    updateVisibleReviewsCount(reviewCards);
+}
+
+function processReviewCard(reviewCard) {
+    if (!filterEnabled.value) {
+        showElement(reviewCard);
+        return;
+    }
+
+    const productVariationWrap = getFirstElement(SELECTORS.PRODUCT_VARIATION_WRAP, reviewCard);
+    const reviewTextWrap = getFirstElement(SELECTORS.REVIEW_TEXT_WRAP, reviewCard);
+
+    if (!productVariationWrap || !reviewTextWrap) {
+        hideElement(reviewCard);
+        return;
+    }
+
+    removeHighlights(reviewCard);
+
+    if (textFilter.value) {
+        highlightSearchStringsByFilterMultiple(
+            textFilter, [productVariationWrap, reviewTextWrap],
+        );
+    }
+
+    const ratingWrap = getFirstElement(SELECTORS.REVIEW_RATING_WRAP, reviewCard);
+
+    if (!ratingWrap) {
+        hideElement(reviewCard);
+        return;
+    }
+
+    const rating = getRating(ratingWrap);
+    const filterableText = reviewTextWrap.innerText + productVariationWrap.innerText;
+
+    const shouldHide =
+        isNotMatchTextFilter(filterableText, textFilter) ||
+        isLessThanFilter(rating, minRatingFilter) ||
+        isGreaterThanFilter(rating, maxRatingFilter);
+    updateElementDisplay(reviewCard, shouldHide);
+}
+
+function getRating(ratingWrap) {
+    const ratingClass = ratingWrap.className;
+    const match = ratingClass.match(/star(\d)/);
+    return match ? +match[1] : 0;
+}
+
+function updateVisibleReviewsCount(reviewCards) {
+    const visibleReviewsCount = [...reviewCards].filter(
+        (reviewCard) => reviewCard.style.display !== 'none',
+    ).length;
+
+    const stickyReviewsInfo = getFirstElement(SELECTORS.STICKY_REVIEWS_INFO);
+    if (!stickyReviewsInfo) return;
+
+    const baseReviewsInfoText = stickyReviewsInfo.textContent.split('(')[0].trim();
+    stickyReviewsInfo.textContent = `${baseReviewsInfoText} (${visibleReviewsCount}/${reviewCards.length})`;
+}
---
+++ src/wb/pages/reviews/selectors.js
@@ -0,0 +1,9 @@
+export const SELECTORS = {
+    CONTROLS_CONTAINER: '.product-feedbacks__sorting',
+    REVIEWS_LIST: '.comments__list',
+    REVIEWS: '.comments__item',
+    PRODUCT_VARIATION_WRAP: '.feedback__params',
+    REVIEW_TEXT_WRAP: '.feedback__content',
+    REVIEW_RATING_WRAP: '.feedback__rating',
+    STICKY_REVIEWS_INFO: '.product-line__param',
+};
