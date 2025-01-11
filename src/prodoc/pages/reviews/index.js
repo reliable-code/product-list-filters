3fec8fd25f80d3328221e432f889aeceac8abeb4
Stepan <stepan.saitov@gmail.com>
2025-01-11 23:04:17 +0500

    Add filters to reviews

--- src/prodoc/pages/reviews/index.js
+++ src/prodoc/pages/reviews/index.js
@@ -6,6 +6,7 @@ import {
 } from '../common';
 import { SELECTORS } from './selectors';
 import {
+    applyStyles,
     getAllElements,
     getElementInnerNumber,
     getFirstElement,
@@ -13,9 +14,34 @@ import {
 import { waitForElement } from '../../../common/dom/utils';
 import { getURLQueryStringParam } from '../../../common/url';
 import { setStoredReviewsData } from '../../db';
+import { createFilterFactory } from '../../../common/filter/factories/createFilter';
+import { appendFilterControlsIfNeeded } from '../../../common/filter/manager';
+import { STYLES } from '../common/styles';
+import {
+    createEnabledFilterControl,
+    createSearchFilterControl,
+} from '../../../common/filter/factories/specificControls';
+import { hideElement, showElement, updateElementDisplay } from '../../../common/dom/manipulation';
+import { removeHighlights } from '../../../common/dom/highlighting';
+import { highlightSearchStringsByFilter } from '../../../common/filter/highlighting';
+import { isNotMatchTextFilter } from '../../../common/filter/compare';
+
+const { createGlobalFilter } = createFilterFactory(processReviewCards);
+
+const reviewTextFilter = createGlobalFilter('reviews-text-filter');
+const filterEnabled = createGlobalFilter('reviews-filter-enabled', true);
+
+const state = {
+    reviewCardsCache: new Map(),
+};
 
 export async function initReviewsMods() {
     appendDoctorPageAdditionalLinks();
+
+    const controlsContainer = await waitForElement(document, SELECTORS.CONTROLS_CONTAINER);
+    appendFilterControlsIfNeeded(controlsContainer, appendFiltersContainer);
+
+    processReviewCards();
 
     const reviewsData = await getReviewsData();
 
@@ -25,6 +51,68 @@ export async function initReviewsMods() {
     appendReviewsInfoBlockToHeader(reviewsInfoBlock);
 
     if (getURLQueryStringParam('rates_category')) scrollToReviews();
+}
+
+function appendFiltersContainer(filtersContainer, parentNode) {
+    applyStyles(filtersContainer, STYLES.FILTERS_CONTAINER);
+
+    const reviewTextFilterDiv = createSearchFilterControl(
+        reviewTextFilter,
+        STYLES.CONTROL,
+        STYLES.TEXT_INPUT,
+    );
+    const filterEnabledDiv = createEnabledFilterControl(
+        filterEnabled,
+        STYLES.CONTROL,
+        STYLES.CHECKBOX_INPUT,
+    );
+
+    filtersContainer.append(
+        reviewTextFilterDiv,
+        filterEnabledDiv,
+    );
+
+    parentNode.append(filtersContainer);
+}
+
+function processReviewCards() {
+    const reviewCards = getAllElements(SELECTORS.REVIEWS);
+    reviewCards.forEach(processReviewCard);
+}
+
+function processReviewCard(reviewCard) {
+    if (!filterEnabled.value) {
+        showElement(reviewCard);
+        return;
+    }
+
+    removeHighlights(reviewCard);
+
+    let cachedData = state.reviewCardsCache.get(reviewCard);
+
+    if (!cachedData) {
+        const reviewTextWrap = getFirstElement(SELECTORS.REVIEW_TEXT_WRAP, reviewCard);
+
+        if (!reviewTextWrap) {
+            hideElement(reviewCard);
+            return;
+        }
+
+        cachedData = {
+            reviewTextWrap,
+            reviewText: reviewTextWrap.innerText,
+        };
+
+        state.reviewCardsCache.set(reviewCard, cachedData);
+    }
+
+    if (reviewTextFilter.value) {
+        highlightSearchStringsByFilter(reviewTextFilter, cachedData.reviewTextWrap);
+    }
+
+    const shouldHide =
+        isNotMatchTextFilter(cachedData.reviewText, reviewTextFilter);
+    updateElementDisplay(reviewCard, shouldHide);
 }
 
 async function getReviewsData() {
--- src/prodoc/pages/reviews/selectors.js
+++ src/prodoc/pages/reviews/selectors.js
@@ -1,4 +1,7 @@
 export const SELECTORS = {
+    CONTROLS_CONTAINER: '.vue-review-filter',
+    REVIEWS: '.b-review-card',
+    REVIEW_TEXT_WRAP: '.b-review-card__comment',
     FILTER_CHIP: '[data-qa="reviews_filter_chip"]',
     FILTER_LIST: '[data-qa="reviews_filter_list"]',
     FILTER_LIST_ITEM: '[data-qa^="reviews_filter_list_item"]',
