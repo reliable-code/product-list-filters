6b6862557677e84cc296a8cf36ebb0f8645a255b
Stepan <stepan.saitov@gmail.com>
2025-01-14 23:33:46 +0500

    Additional styles

--- src/ozon/pages/productPage/styles.js
+++ src/ozon/pages/productPage/styles.js
@@ -1,7 +1,7 @@
 export const STYLES = {
     BAD_REVIEWS_LINK: {
         alignItems: 'center',
-        display: 'flex',
+        display: 'inline-flex',
         color: 'rgba(0, 26, 52, 0.6)',
     },
     BAD_REVIEWS_LINK_SPAN: {
