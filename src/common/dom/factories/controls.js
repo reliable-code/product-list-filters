4cfe78410ff30d770adf4c696392bec4a185ddc4
Stepan <stepan.saitov@gmail.com>
2025-02-04 04:07:23 +0500

    Add createControl

--- src/common/dom/factories/controls.js
+++ src/common/dom/factories/controls.js
@@ -1,1 +1,7 @@
 import { createActionLink, createActionLinkWithIcon, createDiv } from './elements';
+
+function createControl(element, controlStyles = {}) {
+    const control = createDiv(controlStyles);
+    control.append(element);
+    return control;
+}
