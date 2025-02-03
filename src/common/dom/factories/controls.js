1df19b2dac10bbd9153eac6ea4c2cdd832972ed9
Stepan <stepan.saitov@gmail.com>
2025-02-04 04:07:43 +0500

    Add createActionLinkControl and createActionLinkWithIconControl

--- src/common/dom/factories/controls.js
+++ src/common/dom/factories/controls.js
@@ -5,3 +5,24 @@ function createControl(element, controlStyles = {}) {
     control.append(element);
     return control;
 }
+
+export function createActionLinkControl(
+    onClick,
+    innerHTML = null,
+    controlStyles = {},
+    linkStyles = {},
+) {
+    const link = createActionLink(onClick, innerHTML, linkStyles);
+    return createControl(link, controlStyles);
+}
+
+export function createActionLinkWithIconControl(
+    onClick,
+    innerHTML = null,
+    iconName = null,
+    controlStyles = {},
+    linkStyles = {},
+) {
+    const link = createActionLinkWithIcon(onClick, iconName, innerHTML, linkStyles);
+    return createControl(link, controlStyles);
+}
