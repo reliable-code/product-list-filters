8c2ff1c912668402bdfe63ebadc76bb8ed17b1c0
Stepan <stepan.saitov@gmail.com>
2025-04-14 17:38:07 +0500

    New PROMO_LABEL selector

--- src/lavka/selectors.js
+++ src/lavka/selectors.js
@@ -5,5 +5,5 @@ export const SELECTORS = {
     PRODUCT_CARD_LINK: '[data-type="product-card-link"]',
     PRODUCT_CARD_PRICE_WRAP: 'span [aria-hidden="true"]',
     PRODUCT_CARD_PLUS_POINTS: 'span [data-testid="plus-points-value"]',
-    PROMO_LABEL: 'li',
+    PROMO_LABEL: '[data-testid="product-sticker"]',
 };
