4855b9620fc3cea8b126ac70e058faf588218474
Stepan <stepan.saitov@gmail.com>
2025-04-17 17:45:38 +0500

    Update selectors

--- src/ozon/pages/common/selectors.js
+++ src/ozon/pages/common/selectors.js
@@ -1,12 +1,12 @@
 export const SELECTORS = {
     SEARCH_RESULTS_SORT: '[data-widget="searchResultsSort"]',
-    PRODUCT_CARDS: '.widget-search-result-container > div > div.tile-root',
-    CLONED_PRODUCT_CARDS: '#clonedProductCardsWrap > div.tile-root',
+    PRODUCT_CARDS: '.widget-search-result-container .tile-root, #contentScrollPaginator .tile-root',
+    CLONED_PRODUCT_CARDS: '#clonedProductCardsWrap > .tile-root',
     PRODUCT_CARD_NAME_WRAP: '.tsBody500Medium',
     PRODUCT_CARD_PRICE_WRAP: '.tsHeadline500Medium',
     PRODUCT_CARD_RATING_CONTAINER: '.tsBodyMBold',
     PRODUCT_CARD_REVIEWS_WRAP: ':scope > span:nth-of-type(2)',
     PRODUCT_CARD_RATING_WRAP: ':scope > span:nth-of-type(1) > span:nth-of-type(1)',
-    SEARCH_RESULTS_CONTAINER: '.widget-search-result-container',
-    PRODUCT_CARDS_WRAP: '.widget-search-result-container > :first-child',
+    SEARCH_RESULTS_CONTAINER: '.widget-search-result-container, #contentScrollPaginator',
+    PRODUCT_CARDS_WRAP: '#contentScrollPaginator [data-widget="tileGridDesktop"]',
 };
