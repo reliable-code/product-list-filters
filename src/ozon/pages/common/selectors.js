5103576e2200ba7dbc80e580c0afcfebd9a502cb
Stepan <stepan.saitov@gmail.com>
2025-04-17 18:45:33 +0500

    Simplify selectors

--- src/ozon/pages/common/selectors.js
+++ src/ozon/pages/common/selectors.js
@@ -1,6 +1,6 @@
 export const SELECTORS = {
     SEARCH_RESULTS_SORT: '[data-widget="searchResultsSort"]',
-    PRODUCT_CARDS: '.widget-search-result-container .tile-root, #contentScrollPaginator .tile-root',
+    PRODUCT_CARDS: '.tile-root',
     CLONED_PRODUCT_CARDS: '#clonedProductCardsWrap > .tile-root',
     PRODUCT_CARD_NAME_WRAP: '.tsBody500Medium',
     PRODUCT_CARD_PRICE_WRAP: '.tsHeadline500Medium',
@@ -8,5 +8,5 @@ export const SELECTORS = {
     PRODUCT_CARD_REVIEWS_WRAP: ':scope > span:nth-of-type(2)',
     PRODUCT_CARD_RATING_WRAP: ':scope > span:nth-of-type(1) > span:nth-of-type(1)',
     SEARCH_RESULTS_CONTAINER: '.widget-search-result-container, #contentScrollPaginator',
-    PRODUCT_CARDS_WRAP: '#contentScrollPaginator [data-widget="tileGridDesktop"]',
+    PRODUCT_CARDS_WRAP: '[data-widget="tileGridDesktop"]',
 };
